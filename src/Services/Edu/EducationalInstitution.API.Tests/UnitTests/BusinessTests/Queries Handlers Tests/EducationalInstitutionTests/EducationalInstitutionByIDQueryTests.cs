f371912294a037b8ab130b7cae3222a8fe2f6b96
Vlad Bulhac <vladutbulhac@gmail.com>
2021-03-13 14:58:49 +0200

    One test fails

--- src/Services/Edu/EducationalInstitution.API.Tests/UnitTests/BusinessTests/Queries Handlers Tests/EducationalInstitutionTests/EducationalInstitutionByIDQueryTests.cs
+++ src/Services/Edu/EducationalInstitution.API.Tests/UnitTests/BusinessTests/Queries Handlers Tests/EducationalInstitutionTests/EducationalInstitutionByIDQueryTests.cs
@@ -194,7 +194,7 @@ namespace EducationalInstitution.API.Tests
 
             #region Assert
 
-            Assert.True(result.OperationStatus);
+            Assert.False(result.OperationStatus);
 
             #endregion Assert
         }
