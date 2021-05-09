a3874f0baee119d8d3aaed297d8f741b4f169c78
Vlad Bulhac <vladutbulhac@gmail.com>
2021-05-09 22:48:26 +0300

    added GetAllEducationalInstitutionAdminsQueryResult

---
+++ src/Services/Edu/EducationalInstitution.API/Data/Queries and Commands Results/Queries Results/GetAllEducationalInstitutionAdminsQueryResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EducationalInstitutionAPI.Data.Queries_and_Commands_Results.Queries_Results
+{
+    public record GetAllEducationalInstitutionAdminsQueryResult
+    {
+        public ICollection<Guid> AdminsIDs { get; init; }
+    }
+}
