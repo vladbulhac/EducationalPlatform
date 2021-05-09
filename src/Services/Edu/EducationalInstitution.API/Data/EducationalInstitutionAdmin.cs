e621336790287ba20b661254b804941cf1b643f3
Vlad Bulhac <vladutbulhac@gmail.com>
2021-05-09 22:44:39 +0300

    added EducationalInstitutionAdmin

---
+++ src/Services/Edu/EducationalInstitution.API/Data/EducationalInstitutionAdmin.cs
@@ -0,0 +1,24 @@
+﻿using EducationalInstitutionAPI.Data.Helpers;
+using System;
+
+namespace EducationalInstitutionAPI.Data
+{
+    public class EducationalInstitutionAdmin
+    {
+        public Guid AdminID { get; init; }
+        public Guid EducationalInstitutionID { get; init; }
+        public EducationalInstitution EducationalInstitution { get; init; }
+        public Access EntityAccess { get; private set; }
+
+        public EducationalInstitutionAdmin()
+        {
+        }
+
+        public EducationalInstitutionAdmin(Guid adminID, Guid educationalInstitutionID)
+        {
+            AdminID = adminID;
+            EducationalInstitutionID = educationalInstitutionID;
+            EntityAccess = new();
+        }
+    }
+}
