e05548f6ff03397dec58a8369a26a734895e2fbc
Vlad Bulhac <vladutbulhac@gmail.com>
2021-05-16 18:17:52 +0300

    added integration event for admin notification

---
+++ src/Services/Edu/EducationalInstitution.API/Data/Events Definitions/AssignedAdminsToEducationalInstitutionIntegrationEvent.cs
@@ -0,0 +1,11 @@
+﻿using RabbitMQEventBus.IntegrationEvents;
+using System;
+using System.Collections.Generic;
+
+namespace EducationalInstitutionAPI.Business.IntegrationEvents_Handlers
+{
+    public record AssignedAdminsToEducationalInstitutionIntegrationEvent : IntegrationEvent
+    {
+        public ICollection<Guid> ToNotify { get; init; }
+    }
+}
