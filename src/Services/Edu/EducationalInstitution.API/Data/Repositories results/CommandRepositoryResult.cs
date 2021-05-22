a93e94aa824a38c4c2c9b13681e46ffaee0cc408
Vlad Bulhac <vladutbulhac@gmail.com>
2021-05-22 21:00:07 +0300

    added records that return information needed for publishing events to eventbus

---
+++ src/Services/Edu/EducationalInstitution.API/Data/Repositories results/CommandRepositoryResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EducationalInstitutionAPI.Data.Repositories_results
+{
+    public record CommandRepositoryResult
+    {
+        public ICollection<Guid> AdminsToNotify { get; init; }
+
+        public CommandRepositoryResult(ICollection<Guid> adminsToNotify) => AdminsToNotify = adminsToNotify;
+    }
+}
---
+++ src/Services/Edu/EducationalInstitution.API/Data/Repositories results/DeleteCommandRepositoryResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EducationalInstitutionAPI.Data.Repositories_results
+{
+    public record DeleteCommandRepositoryResult : CommandRepositoryResult
+    {
+        public DateTime ScheduledDateForDeletion { get; init; }
+
+        public DeleteCommandRepositoryResult(DateTime deletionDate, ICollection<Guid> adminsToNotify) : base(adminsToNotify)
+                            => ScheduledDateForDeletion = deletionDate;
+    }
+}
