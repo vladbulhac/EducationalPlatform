e599ae34f6f9d46d9911deb9ec6554289e0a3930
Vlad Bulhac <vladutbulhac@gmail.com>
2021-05-26 19:24:42 +0300

    added UpdateEducationalInstitutionAsync, UpdateEducationalInstitutionParentAsync, UpdateEducationalInstitutitonLocationAsync, and DeleteEducationalInstitutionAsync methods

--- src/APIGateway/Aggregator/Services/EducationalInstitution/EducationalInstitutionCommandService.cs
+++ src/APIGateway/Aggregator/Services/EducationalInstitution/EducationalInstitutionCommandService.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Aggregator.Services.EducationalInstitution
 {
-    public class EducationalInstitutionCommandService : GrpcServiceBase<EducationalInstitutionCommandService>, IEducationalInstitutionCommandService
+    public class EducationalInstitutionCommandService : GrpcServiceBase<EducationalInstitutionCommandService>,
+                                                        IEducationalInstitutionCommandService
     {
         private readonly Command.CommandClient client;
 
@@ -18,6 +19,42 @@ namespace Aggregator.Services.EducationalInstitution
             logger.LogDebug("gRPC client created, calls server with request: {@request}", request);
 
             var request_call = client.CreateEducationalInstitutionAsync(request);
+
+            return await MakeUnaryCallAndGetResponseAsync(request_call);
+        }
+
+        public async Task<GrpcCallResponse<EducationalInstitutionUpdateResponse>> UpdateEducationalInstitutionAsync(EducationalInstitutionUpdateRequest request)
+        {
+            logger.LogDebug("gRPC client created, calls server with request: {@request}", request);
+
+            var request_call = client.UpdateEducationalInstitutionAsync(request);
+
+            return await MakeUnaryCallAndGetResponseAsync(request_call);
+        }
+
+        public async Task<GrpcCallResponse<EducationalInstitutionUpdateResponse>> UpdateEducationalInstitutionParentAsync(EducationalInstitutionParentUpdateRequest request)
+        {
+            logger.LogDebug("gRPC client created, calls server with request: {@request}", request);
+
+            var request_call = client.UpdateEducationalInstitutionParentAsync(request);
+
+            return await MakeUnaryCallAndGetResponseAsync(request_call);
+        }
+
+        public async Task<GrpcCallResponse<EducationalInstitutionUpdateResponse>> UpdateEducationalInstitutionLocationAsync(EducationalInstitutionLocationUpdateRequest request)
+        {
+            logger.LogDebug("gRPC client created, calls server with request: {@request}", request);
+
+            var request_call = client.UpdateEducationalInstitutionLocationAsync(request);
+
+            return await MakeUnaryCallAndGetResponseAsync(request_call);
+        }
+
+        public async Task<GrpcCallResponse<EducationalInstitutionDeleteResponse>> DeleteEducationalInstitutionAsync(EducationalInstitutionDeleteRequest request)
+        {
+            logger.LogDebug("gRPC client created, calls server with request: {@request}", request);
+
+            var request_call = client.DeleteEducationalInstitutionAsync(request);
 
             return await MakeUnaryCallAndGetResponseAsync(request_call);
         }
--- src/APIGateway/Aggregator/Services/EducationalInstitution/IEducationalInstitutionCommandService.cs
+++ src/APIGateway/Aggregator/Services/EducationalInstitution/IEducationalInstitutionCommandService.cs
@@ -7,5 +7,13 @@ namespace Aggregator.Services.EducationalInstitution
     public interface IEducationalInstitutionCommandService
     {
         public Task<GrpcCallResponse<EducationalInstitutionCreateResponse>> CreateEducationalInstitutionAsync(EducationalInstitutionCreateRequest request);
+
+        public Task<GrpcCallResponse<EducationalInstitutionUpdateResponse>> UpdateEducationalInstitutionAsync(EducationalInstitutionUpdateRequest request);
+
+        public Task<GrpcCallResponse<EducationalInstitutionUpdateResponse>> UpdateEducationalInstitutionLocationAsync(EducationalInstitutionLocationUpdateRequest request);
+
+        public Task<GrpcCallResponse<EducationalInstitutionUpdateResponse>> UpdateEducationalInstitutionParentAsync(EducationalInstitutionParentUpdateRequest request);
+
+        public Task<GrpcCallResponse<EducationalInstitutionDeleteResponse>> DeleteEducationalInstitutionAsync(EducationalInstitutionDeleteRequest request);
     }
 }
