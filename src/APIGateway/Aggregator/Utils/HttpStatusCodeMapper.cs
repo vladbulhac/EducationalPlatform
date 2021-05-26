36929cad0f31762b9c7af8bc214b16a57fecb18a
Vlad Bulhac <vladutbulhac@gmail.com>
2021-05-26 19:28:34 +0300

    refactored, removed MapToEquivalentHttpStatusCodeOrOK extension methods, added ConvertToHttpStatusCode extension method

--- src/APIGateway/Aggregator/Utils/HttpStatusCodeMapper.cs
+++ src/APIGateway/Aggregator/Utils/HttpStatusCodeMapper.cs
@@ -14,25 +14,17 @@ namespace Aggregator.Utils
         /// <summary>
         /// Extension method that maps a <see cref="ProtoHttpStatusCode"/> to an equivalent <see cref="HttpStatusCode"/> if it can otherwise it maps to <see cref="HttpStatusCode.OK"/>
         /// </summary>
-        public static void MapToEquivalentHttpStatusCodeOrOK(this ProtoHttpStatusCode code, out HttpStatusCode httpStatusCode)
+        public static HttpStatusCode ConvertToHttpStatusCode(this ProtoHttpStatusCode code)
         {
-            if (!httpStatusCodeToProtoMap.ContainsKey(code))
+            HttpStatusCode httpStatusCode;
+            if (!httpStatusCodeToProtoMap.TryGetValue(code, out httpStatusCode))
             {
                 if (Enum.TryParse(code.ToString(), out httpStatusCode))
                     httpStatusCodeToProtoMap.TryAdd(code, httpStatusCode);
                 else
                     httpStatusCode = HttpStatusCode.OK;
             }
-            else
-                httpStatusCodeToProtoMap.TryGetValue(code, out httpStatusCode);
-        }
 
-        /// <summary>
-        /// Extension method that maps a <see cref="ProtoHttpStatusCode"/> to an equivalent <see cref="HttpStatusCode"/> if it can otherwise it maps to <see cref="HttpStatusCode.OK"/>
-        /// </summary>
-        public static HttpStatusCode MapToEquivalentProtoHttpStatusCodeOrOK(this ProtoHttpStatusCode code)
-        {
-            code.MapToEquivalentHttpStatusCodeOrOK(out HttpStatusCode httpStatusCode);
             return httpStatusCode;
         }
     }
