1782c1f4167ea7db16a9dcc60a17ac6fd168b4c1
Vlad Bulhac <vladutbulhac@gmail.com>
2021-05-31 13:59:22 +0300

    added UpdateEducationalInstitutionAdmin method

--- src/Services/Edu/EducationalInstitution.API/Grpc/EducationalInstitutionCommandService.cs
+++ src/Services/Edu/EducationalInstitution.API/Grpc/EducationalInstitutionCommandService.cs
@@ -208,11 +208,71 @@ namespace EducationalInstitutionAPI.Grpc
                 HandleException(logger,
                                 ref context,
                                 "Could not the Educational Institution with the request data: {0}, using {1}, error details=> {2}",
-                                mediator.GetType(),
                                 JsonConvert.SerializeObject(request),
+                                mediator.GetType(),
                                 e.Message);
             }
 
+            return new();
+        }
+
+        /// <summary>
+        /// Overrides the auto generated Remote Call Procedure method from proto file, validates the request fields and sends it to the <see cref="Mediator"/> to handle it
+        /// </summary>
+        /// <returns>
+        /// In addition to the returned <see cref="HttpStatusCode">HttpStatusCodes</see> by <see cref="UpdateEducationalInstitutionAdminCommandHandler">handler</see>:
+        /// <list type="bullet">
+        /// <item><see cref="HttpStatusCode.BadRequest">BadRequest</see> if <paramref name="request"/>'s fields fail the validation process</item>
+        /// </list>
+        /// <see cref="ServerCallContext"/>'s Status is also set, before returning from method, to:
+        /// <list type="bullet">
+        /// <item><see cref="StatusCode.OK">OK</see> if successful</item>
+        /// <item><see cref="StatusCode.InvalidArgument">InvalidArgument</see> if the validation process fails</item>
+        /// <item><see cref="StatusCode.Aborted">Aborted</see> if the request fails or an exception is caught</item>
+        /// </list>
+        /// If the request fails (e.g an Exception is thrown somewhere) then <see cref="ServerCallContext"/>'s ResponseTrailers are set with a message and <see cref="HttpStatusCode"/>
+        /// </returns>
+        public override async Task<EducationalInstitutionUpdateResponse> UpdateEducationalInstitutionAdmin(EducationalInstitutionAdminUpdateRequest request, ServerCallContext context)
+        {
+            logger.LogInformation("Begin grpc call EducationalInstitutionCommandService.UpdateEducationalInstitutionAdmin");
+
+            if (request is null) throw new ArgumentNullException(nameof(request));
+            if (context is null) throw new ArgumentNullException(nameof(context));
+
+            var mappedRequest = request.MapToDTOEducationalInstitutionAdminUpdateCommand();
+
+            if (!validationHandler.IsRequestValid(mappedRequest, out string validationErrors))
+            {
+                SetStatusAndTrailersOfContextWhenValidationFails(ref context, validationErrors);
+                return new();
+            }
+
+            try
+            {
+                var result = await mediator.Send(mappedRequest);
+
+                if (result.OperationStatus)
+                {
+                    context.Status = new(result.StatusCode.ToRPCCallContextStatusCode(), "Educational Institution's admins were successfully updated!");
+                    return new()
+                    {
+                        Message = result.Message,
+                        OperationStatus = result.OperationStatus,
+                        StatusCode = result.StatusCode.MapToEquivalentProtoHttpStatusCodeOrOK()
+                    };
+                }
+                else
+                    SetStatusAndTrailersOfContext(ref context, result.StatusCode.ToRPCCallContextStatusCode(), result.Message, result.StatusCode);
+            }
+            catch (Exception e)
+            {
+                HandleException(logger,
+                                ref context,
+                                "Could not update the admins of the Educational Institution with the request data: {0}, using {1}, error details=> {2}",
+                                JsonConvert.SerializeObject(request),
+                                mediator.GetType(),
+                                e.Message);
+            }
             return new();
         }
 
@@ -269,8 +329,8 @@ namespace EducationalInstitutionAPI.Grpc
                 HandleException(logger,
                                 ref context,
                                 "Could not update the parent of the Educational Institution with the request data: {0}, using {1}, error details=> {2}",
-                                mediator.GetType(),
                                 JsonConvert.SerializeObject(request),
+                                mediator.GetType(),
                                 e.Message);
             }
 
@@ -330,8 +390,8 @@ namespace EducationalInstitutionAPI.Grpc
                 HandleException(logger,
                                ref context,
                                "Could not update the location of the Educational Institution with the request data: {0}, using {1}, error details=> {2}",
-                               mediator.GetType(),
                                JsonConvert.SerializeObject(request),
+                               mediator.GetType(),
                                e.Message);
             }
 
