6450b1cdf2a0dd4da35d056d54cfc9ef08f0e27c
Vlad Bulhac <vladutbulhac@gmail.com>
2021-06-01 22:45:55 +0300

    added

---
+++ src/Services/Edu/EducationalInstitution.API.UnitTests/Grpc Tests/SetupRPCServicesHelper.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+
+namespace EducationalInstitution.API.UnitTests.Grpc_Tests
+{
+    public abstract class SetupRPCServicesHelper<TTestedClass> where TTestedClass : class
+    {
+        protected readonly MockDependenciesHelper<TTestedClass> dependenciesHelper;
+
+        protected SetupRPCServicesHelper(MockDependenciesHelper<TTestedClass> dependenciesHelper)
+        {
+            this.dependenciesHelper = dependenciesHelper;
+        }
+
+        protected void SetupMockedDependenciesToFailValidation<DTO>() where DTO : class
+        {
+            dependenciesHelper.mockValidationHandler.Setup(vh => vh.IsRequestValid(It.IsAny<DTO>(), out It.Ref<string>.IsAny))
+                                                      .Returns(false);
+        }
+    }
+}
