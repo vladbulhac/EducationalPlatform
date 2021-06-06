abb65526b9530928f1799dfde737540e6ebfd150
Vlad Bulhac <vladutbulhac@gmail.com>
2021-06-06 22:06:44 +0300

    added

---
+++ src/Services/Edu/EducationalInstitution.API/Data/Events Definitions/NotificationIntegrationEvent.cs
@@ -0,0 +1,11 @@
+﻿using RabbitMQEventBus.IntegrationEvents;
+using System;
+using System.Collections.Generic;
+
+namespace EducationalInstitutionAPI.Data.Events_Definitions
+{
+    public record NotificationIntegrationEvent : IntegrationEvent
+    {
+        public ICollection<Guid> ToNotify { get; init; }
+    }
+}
