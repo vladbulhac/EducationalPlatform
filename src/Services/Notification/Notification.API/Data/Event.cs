90639ec458111b782250e6df263de471882ded85
Vlad Bulhac <vladutbulhac@gmail.com>
2021-06-06 22:19:59 +0300

    added Name property

--- src/Services/Notification/Notification.API/Data/Event.cs
+++ src/Services/Notification/Notification.API/Data/Event.cs
@@ -7,15 +7,13 @@ namespace Notification.API.Data
     {
         public Guid EventID { get; private set; }
         public DateTime TimeIssued { get; init; }
+        public string Name { get; init; }
         public string Message { get; init; }
         public string TriggeredByAction { get; init; }
         public string Url { get; init; }
         public ICollection<Recipient> Recipients { get; set; }
         public string IssuedBy { get; init; }
 
-        public Event()
-        {
-            EventID = Guid.NewGuid();
-        }
+        public Event() => EventID = Guid.NewGuid();
     }
 }
