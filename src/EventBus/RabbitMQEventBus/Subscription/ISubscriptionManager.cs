351f0402dc2cd4040514a9fc2c3247f1b01d4d83
Vlad Bulhac <vladutbulhac@gmail.com>
2021-06-09 23:53:30 +0300

    added

---
+++ src/EventBus/RabbitMQEventBus/Subscription/ISubscriptionManager.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQEventBus.IntegrationEvents;
+using System;
+using System.Collections.Generic;
+
+namespace RabbitMQEventBus.Subscription
+{
+    /// <summary>
+    /// Keeps track of which <see cref="IIntegrationEventHandler{TIntegrationEvent}"/> handles which <see cref="IntegrationEvent"/>
+    /// </summary>
+    public interface ISubscriptionManager
+    {
+        public void SaveSubscription(Type eventType, Type handler);
+
+        public ICollection<Type> GetHandlersOfEvent(string eventType);
+
+        public bool HasSubscription(string eventType);
+
+        public Type GetTypeOfEvent(string eventName);
+    }
+}
---
+++ src/EventBus/RabbitMQEventBus/Subscription/SubscriptionManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RabbitMQEventBus.Subscription
+{
+    /// <inheritdoc cref="ISubscriptionManager"/>
+    public class SubscriptionManager : ISubscriptionManager
+    {
+        private readonly IDictionary<string, HashSet<Type>> eventNameToHandlersMap;
+        private readonly IDictionary<string, Type> eventNameToTypeMap;
+
+        public SubscriptionManager()
+        {
+            eventNameToHandlersMap = new Dictionary<string, HashSet<Type>>();
+            eventNameToTypeMap = new Dictionary<string, Type>();
+        }
+
+        public void SaveSubscription(Type @event, Type handler)
+        {
+            var eventName = @event.Name;
+            if (!eventNameToHandlersMap.ContainsKey(eventName))
+            {
+                eventNameToHandlersMap.Add(eventName, new HashSet<Type>() { handler });
+                eventNameToTypeMap.Add(eventName, @event);
+            }
+            else
+                eventNameToHandlersMap[eventName].Add(handler);
+        }
+
+        public ICollection<Type> GetHandlersOfEvent(string eventName)
+        {
+            if (!eventNameToHandlersMap.ContainsKey(eventName))
+                return default;
+
+            return eventNameToHandlersMap[eventName];
+        }
+
+        public Type GetTypeOfEvent(string eventName)
+        {
+            if (!eventNameToTypeMap.ContainsKey(eventName))
+                return default;
+
+            return eventNameToTypeMap[eventName];
+        }
+
+        public bool HasSubscription(string eventName) => eventNameToHandlersMap.ContainsKey(eventName);
+    }
+}
