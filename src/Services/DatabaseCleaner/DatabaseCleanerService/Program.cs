6e514c96bce5d030e9c29b644232173e48a465b3
Vlad Bulhac <vladutbulhac@gmail.com>
2021-06-11 21:08:00 +0300

    removed CreateHostBuilder method

--- src/Services/DatabaseCleaner/DatabaseCleanerService/Program.cs
+++ src/Services/DatabaseCleaner/DatabaseCleanerService/Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace DatabaseCleanerService
@@ -7,14 +6,9 @@ namespace DatabaseCleanerService
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<Worker>();
-                });
+                .Build()
+                .Run();
+        }
     }
 }
