e8e2557d6c975ca3ce460255ad9a0e825a2c8fed
Vlad Bulhac <vladutbulhac@gmail.com>
2021-06-28 11:46:22 +0300

    added GetSubscriptionDetailsOfEvent method, removed GetHandlersOfEvent, GetTypeOfEvent methods, removed eventNameToHandlersMap and eventNameToTypeMap dictionaries, added eventNameToSubscriptionMap dictionary

--- src/EventBus/RabbitMQEventBus/Subscription/SubscriptionManager.cs
+++ src/EventBus/RabbitMQEventBus/Subscription/SubscriptionManager.cs
@@ -6,43 +6,28 @@ namespace RabbitMQEventBus.Subscription
     /// <inheritdoc cref="ISubscriptionManager"/>
     public class SubscriptionManager : ISubscriptionManager
     {
-        private readonly IDictionary<string, HashSet<Type>> eventNameToHandlersMap;
-        private readonly IDictionary<string, Type> eventNameToTypeMap;
+        private readonly Dictionary<string, Subscription> eventNameToSubscriptionMap;
 
-        public SubscriptionManager()
-        {
-            eventNameToHandlersMap = new Dictionary<string, HashSet<Type>>();
-            eventNameToTypeMap = new Dictionary<string, Type>();
-        }
+        public SubscriptionManager() => eventNameToSubscriptionMap = new();
 
         public void SaveSubscription(Type @event, Type handler)
         {
             var eventName = @event.Name;
-            if (!eventNameToHandlersMap.ContainsKey(eventName))
+
+            switch (HasSubscription(eventName))
             {
-                eventNameToHandlersMap.Add(eventName, new HashSet<Type>() { handler });
-                eventNameToTypeMap.Add(eventName, @event);
+                case true:
+                    eventNameToSubscriptionMap[eventName].Add(handler);
+                    break;
+
+                default:
+                    eventNameToSubscriptionMap.Add(eventName, new Subscription(@event, handler));
+                    break;
             }
-            else
-                eventNameToHandlersMap[eventName].Add(handler);
         }
 
-        public ICollection<Type> GetHandlersOfEvent(string eventName)
-        {
-            if (!eventNameToHandlersMap.ContainsKey(eventName))
-                return default;
+        public Subscription GetSubscriptionDetailsOfEvent(string eventName) => HasSubscription(eventName) ? eventNameToSubscriptionMap[eventName] : default;
 
-            return eventNameToHandlersMap[eventName];
-        }
-
-        public Type GetTypeOfEvent(string eventName)
-        {
-            if (!eventNameToTypeMap.ContainsKey(eventName))
-                return default;
-
-            return eventNameToTypeMap[eventName];
-        }
-
-        public bool HasSubscription(string eventName) => eventNameToHandlersMap.ContainsKey(eventName);
+        private bool HasSubscription(string eventName) => eventNameToSubscriptionMap.ContainsKey(eventName);
     }
 }
