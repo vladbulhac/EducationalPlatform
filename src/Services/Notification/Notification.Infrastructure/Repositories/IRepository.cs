8631dd111dc9c48eb68b492503964ba6563b6e72
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 15:51:48 +0300

    added IRepository

---
+++ src/Services/Notification/Notification.Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using Notification.Domain.Building_Blocks;
+
+namespace Notification.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Encapsulates the logic required to access data sources
+    /// </summary>
+    public interface IRepository<T> where T : IAggregateRoot
+    { }
+}
