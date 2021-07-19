86a2fb5e00aaca6be607fae46f05eb0920ff110e
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 16:40:31 +0300

    moved EducationalInstitutionBuildingEntityTypeConfiguration to Infrastructure, removed index on buildingId-educationalInstitutionId-IsDisabled, configured Id property to have ColumnName: EducationalInstitutionID, configured OwnsOne Access

---
+++ src/Services/Edu/EducationalInstitution.Infrastructure/Entities Type Configuration/EducationalInstitutionBuildingEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using EducationalInstitution.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EducationalInstitution.Infrastructure.Entities_Type_Configuration
+{
+    public class EducationalInstitutionBuildingEntityTypeConfiguration : IEntityTypeConfiguration<EducationalInstitutionBuilding>
+    {
+        public void Configure(EntityTypeBuilder<EducationalInstitutionBuilding> builder)
+        {
+            builder.HasKey(eib => new { eib.BuildingID, eib.Id });
+
+            builder.Property(eib => eib.Id).HasColumnName("EducationalInstitutionID");
+
+            builder.OwnsOne(eib => eib.Access, exp =>
+            {
+                exp.HasIndex(eib => new { eib.IsDisabled });
+                exp.Property(a => a.IsDisabled).HasColumnName("IsDisabled");
+                exp.Property(a => a.DateForPermanentDeletion).HasColumnName("DateForPermanentDeletion");
+            });
+        }
+    }
+}
