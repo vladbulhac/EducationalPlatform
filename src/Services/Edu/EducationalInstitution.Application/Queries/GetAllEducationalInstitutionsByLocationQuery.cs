ed592d56ff864337b65429f195ff279e883ac0dd
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 18:23:09 +0300

    moved DTOEducationalInstitutionsByLocationQuery to Application, renamed to GetAllEducationalInstitutionsByLocationQuery

---
+++ src/Services/Edu/EducationalInstitution.Application/Queries/GetAllEducationalInstitutionsByLocationQuery.cs
@@ -0,0 +1,10 @@
+﻿using EducationalInstitution.Infrastructure.Repositories.Query_Repository.Results;
+using MediatR;
+
+namespace EducationalInstitution.Application.Queries
+{
+    public class GetAllEducationalInstitutionsByLocationQuery : IRequest<Response<GetAllEducationalInstitutionsByLocationQueryResult>>
+    {
+        public string LocationID { get; init; }
+    }
+}
