4bbe786e4719b0735a5ca356d814610bf041281b
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 16:08:37 +0300

    added IAggregateRoot

---
+++ src/Services/Notification/Notification.Domain/Building Blocks/IAggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿namespace Notification.Domain.Building_Blocks
+{
+    /// <summary>
+    /// <para> An aggregate is a cluster of associated objects that are treated as a unit. </para>
+    /// Each aggregate has a root and outside objects can hold references only to it.
+    /// </summary>
+    public interface IAggregateRoot
+    {
+    }
+}
