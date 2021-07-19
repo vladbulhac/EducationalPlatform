cf477f515c6f60533bf5e033dba749a183a7ebbb
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 18:24:04 +0300

    moved DTOEducationalInstitutionsByNameQuery to Application, renamed to GetAllEducationalInstitutionsByNameQuery

---
+++ src/Services/Edu/EducationalInstitution.Application/Queries/GetAllEducationalInstitutionsByNameQuery.cs
@@ -0,0 +1,12 @@
+﻿using EducationalInstitution.Infrastructure.Repositories.Query_Repository.Results;
+using MediatR;
+
+namespace EducationalInstitution.Application.Queries
+{
+    public class GetAllEducationalInstitutionsByNameQuery : IRequest<Response<GetAllEducationalInstitutionsByNameQueryResult>>
+    {
+        public string Name { get; init; }
+        public int OffsetValue { get; init; }
+        public int ResultsCount { get; init; }
+    }
+}
