82fc55fa445aa352cc3d707143b6ecc86532caaf
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 17:27:54 +0300

    moved Access to Domain, extends ValueObject, removed ScheduleForDeletion and RemoveDeletionSchedule methods

---
+++ src/Services/Edu/EducationalInstitution.Domain/Models/Access.cs
@@ -0,0 +1,17 @@
+﻿using EducationalInstitution.Domain.Building_Blocks;
+using System;
+
+namespace EducationalInstitution.Domain.Models
+{
+    public record Access : ValueObject
+    {
+        /// <value>True if the entity is scheduled for deletion, False otherwise</value>
+        public bool IsDisabled { get; init; }
+
+        /// <summary>
+        /// Describes the date at which the entity is completely removed from the data source
+        /// </summary>
+        /// <value>A <see cref="DateTime"/> if IsDisabled is True or NULL value if IsDisabled is False</value>
+        public DateTime? DateForPermanentDeletion { get; init; }
+    }
+}
