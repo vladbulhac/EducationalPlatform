387503779dd803761efdeca3549c6792431671a1
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 16:08:22 +0300

    added Entity

---
+++ src/Services/Notification/Notification.Domain/Building Blocks/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Notification.Domain.Building_Blocks
+{
+    /// <summary>
+    /// Represents an object that is defined by its identity and not by its attributes.
+    /// </summary>
+    public abstract class Entity
+    {
+        public Guid Id { get; init; }
+
+        protected Entity()
+        { }
+
+        protected Entity(Guid? id)
+        {
+            if (id.HasValue && id == Guid.Empty) throw new ArgumentException($"{nameof(id)} doesn't have a valid value!");
+
+            Id = id ?? Guid.NewGuid();
+        }
+    }
+}
