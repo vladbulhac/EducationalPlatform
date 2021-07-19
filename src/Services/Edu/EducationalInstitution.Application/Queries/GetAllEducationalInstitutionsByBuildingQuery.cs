30cfbe365966bb6e0b074c51fbc486c756209ceb
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 18:22:08 +0300

    moved DTOEducationalInstitutionsByBuildingQuery to Application, renamed to GetAllEducationalInstitutionsByBuildingQuery

---
+++ src/Services/Edu/EducationalInstitution.Application/Queries/GetAllEducationalInstitutionsByBuildingQuery.cs
@@ -0,0 +1,10 @@
+﻿using EducationalInstitution.Infrastructure.Repositories.Query_Repository.Results;
+using MediatR;
+
+namespace EducationalInstitution.Application.Queries
+{
+    public class GetAllEducationalInstitutionsByBuildingQuery : IRequest<Response<GetAllEducationalInstitutionsWithSameBuildingQueryResult>>
+    {
+        public string BuildingID { get; init; }
+    }
+}
