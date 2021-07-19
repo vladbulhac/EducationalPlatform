58622c8135204edd322ec1c453289d8f3cbdfc38
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 17:30:12 +0300

    moved EducationalInstitutionBuilding to Domain, extends Entity

---
+++ src/Services/Edu/EducationalInstitution.Domain/Models/EducationalInstitutionBuilding.cs
@@ -0,0 +1,19 @@
+﻿using EducationalInstitution.Domain.Building_Blocks;
+using System;
+using Model = EducationalInstitution.Domain.Models.Aggregates;
+
+namespace EducationalInstitution.Domain.Models
+{
+    public class EducationalInstitutionBuilding : Entity
+    {
+        public Model::EducationalInstitution EducationalInstitution { get; init; }
+        public string BuildingID { get; init; }
+
+        public EducationalInstitutionBuilding()
+        {
+        }
+
+        public EducationalInstitutionBuilding(string buildingID, Guid educationalInstitutionID) : base(educationalInstitutionID)
+                => BuildingID = string.IsNullOrEmpty(buildingID) ? throw new ArgumentNullException(nameof(buildingID)) : buildingID;
+    }
+}
