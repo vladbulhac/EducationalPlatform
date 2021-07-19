769babe2e99dba823cfd45073187b681473fe5e3
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 17:45:05 +0300

    moved DTOEducationalInstitutionDeleteCommand to Application, renamed to DisableEducationalInstitutionCommand

---
+++ src/Services/Edu/EducationalInstitution.Application/Commands/DisableEducationalInstitutionCommand.cs
@@ -0,0 +1,11 @@
+﻿using EducationalInstitution.Application.Commands.Results;
+using MediatR;
+using System;
+
+namespace EducationalInstitution.Application.Commands
+{
+    public class DisableEducationalInstitutionCommand : IRequest<Response<DisableEducationalInstitutionCommandResult>>
+    {
+        public Guid EducationalInstitutionID { get; init; }
+    }
+}
