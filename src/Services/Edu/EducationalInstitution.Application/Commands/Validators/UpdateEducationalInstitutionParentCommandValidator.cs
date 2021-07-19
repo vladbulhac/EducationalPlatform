d71983421798c461db76e860109a56fde57b7ba9
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 18:01:45 +0300

    moved DTOEducationalInstitutionParentUpdateCommandValidator to Application, renamed to UpdateEducationalInstitutionParentCommandValidator

---
+++ src/Services/Edu/EducationalInstitution.Application/Commands/Validators/UpdateEducationalInstitutionParentCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace EducationalInstitution.Application.Commands.Validators
+{
+    public class UpdateEducationalInstitutionParentCommandValidator : AbstractValidator<UpdateEducationalInstitutionParentCommand>
+    {
+        public UpdateEducationalInstitutionParentCommandValidator()
+        {
+            CascadeMode = CascadeMode.Stop;
+            RuleFor(dto => dto.EducationalInstitutionID)
+                             .NotEmpty()
+                             .WithMessage("{PropertyName} was empty or null!");
+
+            RuleFor(dto => dto.ParentInstitutionID)
+                             .NotEqual(v => v.EducationalInstitutionID)
+                             .WithMessage(dto => $"{nameof(dto.ParentInstitutionID)} was the same as {nameof(dto.EducationalInstitutionID)}!");
+        }
+    }
+}
