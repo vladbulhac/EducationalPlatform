2d332dd4aecd96d7b34cd9b2fab4b3154d8829e1
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 15:22:43 +0300

    added PublishMultiple method

--- src/EventBus/RabbitMQEventBus/Abstractions/IEventBusPublisher.cs
+++ src/EventBus/RabbitMQEventBus/Abstractions/IEventBusPublisher.cs
@@ -1,9 +1,12 @@
 ﻿using RabbitMQEventBus.IntegrationEvents;
+using System.Collections.Generic;
 
 namespace RabbitMQEventBus.Abstractions
 {
     public interface IEventBusPublisher
     {
         public void Publish(IntegrationEvent @event);
+
+        public void PublishMultiple(IEnumerable<IntegrationEvent> @events);
     }
 }
