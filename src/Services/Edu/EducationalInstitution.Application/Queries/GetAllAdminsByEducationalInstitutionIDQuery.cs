33ec46c45ccbcb5c0392ba4283ee58b7070c9e05
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 18:21:09 +0300

    moved DTOAdminsByEducationalInstitutionQuery to Application, renamed to GetAllAdminsByEducationalInstitutionIDQuery

---
+++ src/Services/Edu/EducationalInstitution.Application/Queries/GetAllAdminsByEducationalInstitutionIDQuery.cs
@@ -0,0 +1,11 @@
+﻿using EducationalInstitution.Infrastructure.Repositories.Query_Repository.Results;
+using MediatR;
+using System;
+
+namespace EducationalInstitution.Application.Queries
+{
+    public class GetAllAdminsByEducationalInstitutionIDQuery : IRequest<Response<GetAllAdminsOfEducationalInstitutionQueryResult>>
+    {
+        public Guid EducationalInstitutionID { get; init; }
+    }
+}
