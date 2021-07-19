c82578a0a5cd2c6be1e51f6c5a184b3e5a66ab96
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 17:47:02 +0300

    moved DTOEducationalInstitutionUpdateCommand to Application, renamed to UpdateEducationalInstitutionCommand

---
+++ src/Services/Edu/EducationalInstitution.Application/Commands/UpdateEducationalInstitutionCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+
+namespace EducationalInstitution.Application.Commands
+{
+    public class UpdateEducationalInstitutionCommand : IRequest<Response>
+    {
+        public Guid EducationalInstitutionID { get; init; }
+
+        public bool UpdateName { get; init; }
+        public string Name { get; init; }
+
+        public bool UpdateDescription { get; init; }
+        public string Description { get; init; }
+    }
+}
