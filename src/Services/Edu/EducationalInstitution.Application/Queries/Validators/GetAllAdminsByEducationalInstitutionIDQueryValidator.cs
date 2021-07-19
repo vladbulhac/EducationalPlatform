da3c15ad64704cf91e8c1187d95cfce4efdb0e4a
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 18:26:22 +0300

    moved DTOAdminsByEducationalInstitutionIDQueryValidator to Application, renamed to GetAllAdminsByEducationalInstitutionIDQueryValidator

---
+++ src/Services/Edu/EducationalInstitution.Application/Queries/Validators/GetAllAdminsByEducationalInstitutionIDQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace EducationalInstitution.Application.Queries.Validators
+{
+    public class GetAllAdminsByEducationalInstitutionIDQueryValidator : AbstractValidator<GetAllAdminsByEducationalInstitutionIDQuery>
+    {
+        public GetAllAdminsByEducationalInstitutionIDQueryValidator()
+        {
+            CascadeMode = CascadeMode.Stop;
+            RuleFor(dto => dto.EducationalInstitutionID)
+                        .NotEmpty()
+                            .WithMessage(dto => $"{nameof(dto.EducationalInstitutionID)} is empty, null or default!");
+        }
+    }
+}
