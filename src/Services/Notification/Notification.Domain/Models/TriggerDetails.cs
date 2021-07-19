802a0c3515bdcc9b4d33015727efdb194b36ba8b
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 16:21:10 +0300

    added TriggerDetails

---
+++ src/Services/Notification/Notification.Domain/Models/TriggerDetails.cs
@@ -0,0 +1,20 @@
+﻿using Notification.Domain.Building_Blocks;
+using System;
+
+namespace Notification.Domain.Models
+{
+    public record TriggerDetails : ValueObject
+    {
+        public string Action { get; init; }
+        public string Issuer { get; init; }
+        public DateTime TimeIssued { get; init; }
+
+        public TriggerDetails(string action, string issuer, DateTime timeIssued)
+        {
+            Action = action ?? throw new ArgumentNullException(nameof(action));
+            Issuer = issuer ?? throw new ArgumentNullException(nameof(issuer));
+
+            TimeIssued = timeIssued;
+        }
+    }
+}
