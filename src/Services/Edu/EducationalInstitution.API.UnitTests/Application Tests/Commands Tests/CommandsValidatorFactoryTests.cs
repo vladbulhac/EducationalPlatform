c67fe047bde535ccaff55ed64eb34ee1ad5b02b9
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 19:34:47 +0300

    split ValidatorFactoryTests into CommandsValidatorFactoryTests and QueriesValidatorFactoryTests

---
+++ src/Services/Edu/EducationalInstitution.API.UnitTests/Application Tests/Commands Tests/CommandsValidatorFactoryTests.cs
@@ -0,0 +1,72 @@
+﻿using DataValidation;
+using EducationalInstitution.Application.Commands;
+using EducationalInstitution.Application.Commands.Validators;
+using Xunit;
+
+namespace EducationalInstitution.API.UnitTests.Application_Tests.Commands_Tests
+{
+    public class CommandsValidatorFactoryTests
+    {
+        private readonly ValidatorFactory validatorFactory = new(typeof(CreateEducationalInstitutionCommandValidator).Assembly);
+
+        [Fact]
+        public void GivenAValidTypeCreateEducationalInstitutionCommand_ShouldReturnAnInstanceOfCreateEducationalInstitutionCommandValidator()
+        {
+            //Act
+            var validator = validatorFactory.CreateValidator<CreateEducationalInstitutionCommand>();
+
+            //Assert
+            Assert.IsType<CreateEducationalInstitutionCommandValidator>(validator);
+        }
+
+        [Fact]
+        public void GivenAValidTypeDisableEducationalInstitutionCommand_ShouldReturnAnInstanceOfDisableEducationalInstitutionCommandValidator()
+        {
+            //Act
+            var validator = validatorFactory.CreateValidator<DisableEducationalInstitutionCommand>();
+
+            //Assert
+            Assert.IsType<DisableEducationalInstitutionCommandValidator>(validator);
+        }
+
+        [Fact]
+        public void GivenAValidTypeUpdateEducationalInstitutionLocationCommand_ShouldReturnAnInstanceOfUpdateEducationalInstitutionLocationCommandValidator()
+        {
+            //Act
+            var validator = validatorFactory.CreateValidator<UpdateEducationalInstitutionLocationCommand>();
+
+            //Assert
+            Assert.IsType<UpdateEducationalInstitutionLocationCommandValidator>(validator);
+        }
+
+        [Fact]
+        public void GivenAValidTypeUpdateEducationalInstitutionParentCommand_ShouldReturnAnInstanceOfUpdateEducationalInstitutionParentCommandValidator()
+        {
+            //Act
+            var validator = validatorFactory.CreateValidator<UpdateEducationalInstitutionParentCommand>();
+
+            //Assert
+            Assert.IsType<UpdateEducationalInstitutionParentCommandValidator>(validator);
+        }
+
+        [Fact]
+        public void GivenAValidTypeUpdateEducationalInstitutionCommand_ShouldReturnAnInstanceOfUpdateEducationalInstitutionCommandValidator()
+        {
+            //Act
+            var validator = validatorFactory.CreateValidator<UpdateEducationalInstitutionCommand>();
+
+            //Assert
+            Assert.IsType<UpdateEducationalInstitutionCommandValidator>(validator);
+        }
+
+        [Fact]
+        public void GivenAValidTypeUpdateEducationalInstitutionAdminsCommand_ShouldReturnAnInstanceOfUpdateEducationalInstitutionAdminsCommandValidator()
+        {
+            //Act
+            var validator = validatorFactory.CreateValidator<UpdateEducationalInstitutionAdminsCommand>();
+
+            //Assert
+            Assert.IsType<UpdateEducationalInstitutionAdminsCommandValidator>(validator);
+        }
+    }
+}
---
+++ src/Services/Edu/EducationalInstitution.API.UnitTests/Application Tests/Queries Tests/QueriesValidatorFactoryTests.cs
@@ -0,0 +1,72 @@
+﻿using DataValidation;
+using DataValidation.Exceptions;
+using EducationalInstitution.Application.Commands.Validators;
+using EducationalInstitution.Application.Queries;
+using EducationalInstitution.Application.Queries.Validators;
+using System.Text;
+using Xunit;
+
+namespace EducationalInstitution.API.UnitTests.Application_Tests.Queries_Tests
+{
+    public class QueriesValidatorFactoryTests
+    {
+        private readonly ValidatorFactory validatorFactory = new(typeof(CreateEducationalInstitutionCommandValidator).Assembly);
+
+        [Fact]
+        public void GivenAValidTypeGetEducationalInstitutionByIDQuery_ShouldReturnAnInstanceOfGetEducationalInstitutionByIDQueryValidator()
+        {
+            //Act
+            var validator = validatorFactory.CreateValidator<GetEducationalInstitutionByIDQuery>();
+
+            //Assert
+            Assert.IsType<GetEducationalInstitutionByIDQueryValidator>(validator);
+        }
+
+        [Fact]
+        public void GivenAValidTypeGetAllEducationalInstitutionsByLocationQuery_ShouldReturnAnInstanceOfGetAllEducationalInstitutionsByLocationQueryValidator()
+        {
+            //Act
+            var validator = validatorFactory.CreateValidator<GetAllEducationalInstitutionsByLocationQuery>();
+
+            //Assert
+            Assert.IsType<GetAllEducationalInstitutionsByLocationQueryValidator>(validator);
+        }
+
+        [Fact]
+        public void GivenAValidTypeGetAllEducationalInstitutionsByNameQuery_ShouldReturnAnInstanceOfGetAllEducationalInstitutionsByNameQueryValidator()
+        {
+            //Act
+            var validator = validatorFactory.CreateValidator<GetAllEducationalInstitutionsByNameQuery>();
+
+            //Assert
+            Assert.IsType<GetAllEducationalInstitutionsByNameQueryValidator>(validator);
+        }
+
+        [Fact]
+        public void GivenAValidTypeGetAllAdminsByEducationalInstitutionIdQuery_ShouldReturnAnInstanceOfGetAllAdminsByEducationalInstitutionIDQueryValidator()
+        {
+            //Act
+            var validator = validatorFactory.CreateValidator<GetAllAdminsByEducationalInstitutionIDQuery>();
+
+            //Assert
+            Assert.IsType<GetAllAdminsByEducationalInstitutionIDQueryValidator>(validator);
+        }
+
+        [Fact]
+        public void GivenAValidTypeGetAllEducationalInstitutionsByBuildingQuery_ShouldReturnAnInstanceOfGetAllEducationalInstitutionsByBuildingQueryValidator()
+        {
+            //Act
+            var validator = validatorFactory.CreateValidator<GetAllEducationalInstitutionsByBuildingQuery>();
+
+            //Assert
+            Assert.IsType<GetAllEducationalInstitutionsByBuildingQueryValidator>(validator);
+        }
+
+        [Fact]
+        public void GivenAnInvalidType_ShouldThrowAnRequestTypeNotSupportedException()
+        {
+            //Assert
+            Assert.Throws<RequestTypeNotSupportedException>(() => validatorFactory.CreateValidator<StringBuilder>());
+        }
+    }
+}
