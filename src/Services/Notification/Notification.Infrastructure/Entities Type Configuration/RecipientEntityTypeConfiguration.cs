2c4a39afbc0ef2f4670606db3d87d85562132047
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 16:06:59 +0300

    moved RecipientEntityTypeConfiguration to Infrastructure

---
+++ src/Services/Notification/Notification.Infrastructure/Entities Type Configuration/RecipientEntityTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notification.Domain.Models;
+
+namespace Notification.Infrastructure.Entities_Type_Configuration
+{
+    public class RecipientEntityTypeConfiguration : IEntityTypeConfiguration<Recipient>
+    {
+        public void Configure(EntityTypeBuilder<Recipient> builder)
+        {
+            builder.HasKey(r => new { r.Id, r.EventID });
+        }
+    }
+}
