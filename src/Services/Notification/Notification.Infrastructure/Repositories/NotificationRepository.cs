b2990aa50610649873d3e2c909d644e7cb7683d9
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 16:00:49 +0300

    moved NotificationRepository to Infrastructure, renamed CreateEvent method to AddEventAsync, renamed GetUnseenEvents method to GetUnseenEventsForRecipientAsync and changed return type to collection of GetEventDetails, renamed RecipientSawTheEvent method to RecipientSawTheEventAsync, added eventId parameter and changed the logic

---
+++ src/Services/Notification/Notification.Infrastructure/Repositories/NotificationRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notification.Domain.Models.Aggregates;
+using Notification.Infrastructure.Repositories.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Notification.Infrastructure.Repositories
+{
+    public class NotificationRepository : INotificationRepository
+    {
+        private readonly NotificationContext context;
+
+        public NotificationRepository(NotificationContext context)
+            => this.context = context ?? throw new ArgumentNullException(nameof(context));
+
+        public async Task AddEventAsync(Event eventEntity, CancellationToken cancellationToken = default)
+        {
+            await context.AddAsync(eventEntity, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<bool> RecipientSawTheEventAsync(Guid eventId, Guid recipientId, CancellationToken cancellationToken = default)
+        {
+            var eventEntity = await context.Events.Include(e => e.Recipients)
+                                                  .SingleOrDefaultAsync(e => e.Id == eventId, cancellationToken);
+            if (eventEntity == default) return false;
+
+            eventEntity.RecipientSawEventNotification(recipientId);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+
+        public async Task<ICollection<GetEventDetails>> GetUnseenEventsForRecipientAsync(Guid recipientID, CancellationToken cancellationToken = default)
+        {
+            return await context.Recipients.Include(r => r.Event)
+                                           .Where(r => r.Id == recipientID && !r.Seen)
+                                           .Select(e => new GetEventDetails()
+                                           {
+                                               Message = e.Event.Message,
+                                               TimeIssued = e.Event.TriggerDetails.TimeIssued,
+                                               Url = e.Event.Url
+                                           })
+                                           .ToListAsync(cancellationToken);
+        }
+    }
+}
