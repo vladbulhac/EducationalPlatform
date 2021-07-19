3a349b1bbae6f607d27cb64fef26fb7690526de4
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 16:06:33 +0300

    moved EventEntityTypeConfiguration to Infrastructure, added OwnsOne TriggerDetails, added new indexes, configured OnDelete behavior when an Event is deleted

---
+++ src/Services/Notification/Notification.Infrastructure/Entities Type Configuration/EventEntityTypeConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notification.Domain.Models.Aggregates;
+
+namespace Notification.Infrastructure.Entities_Type_Configuration
+{
+    public class EventEntityTypeConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.HasIndex(e => new { e.Name, e.Message, e.Url });
+
+            builder.OwnsOne(e => e.TriggerDetails, onb =>
+            {
+                onb.Property(e => e.Action).HasColumnName("TriggeredByAction")
+                                           .HasMaxLength(100)
+                                           .IsRequired();
+
+                onb.Property(e => e.Issuer).HasColumnName("Issuer")
+                                           .HasMaxLength(150)
+                                           .IsRequired();
+
+                onb.Property(e => e.TimeIssued).HasColumnName("TimeIssued")
+                                               .IsRequired();
+
+                onb.HasIndex(e => new { e.Issuer, e.TimeIssued, e.Action });
+            });
+
+            builder.HasMany(e => e.Recipients)
+                    .WithOne(pe => pe.Event)
+                    .HasForeignKey(pe => pe.EventID)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsRequired();
+            builder.Property(e => e.Message)
+                    .HasMaxLength(350)
+                    .IsRequired();
+            builder.Property(e => e.Url)
+                    .HasMaxLength(100)
+                    .IsRequired();
+        }
+    }
+}
