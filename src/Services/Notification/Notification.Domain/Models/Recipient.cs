6ac3ca5dfc3438974641bd333628bf634e29384b
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 16:20:55 +0300

    moved Recipient to Domain, extends Entity, removed RecipientID property, added ChangeSeenStatus method, removed SetNotificationToSeen method

---
+++ src/Services/Notification/Notification.Domain/Models/Recipient.cs
@@ -0,0 +1,27 @@
+﻿using Notification.Domain.Building_Blocks;
+using Notification.Domain.Models.Aggregates;
+using System;
+
+namespace Notification.Domain.Models
+{
+    /// <summary>
+    /// A person or thing that receives a notification
+    /// </summary>
+    public class Recipient : Entity
+    {
+        public Guid EventID { get; init; }
+        public Event Event { get; init; }
+        public bool Seen { get; private set; }
+
+        public Recipient()
+        { }
+
+        public Recipient(Guid personID, Guid eventID) : base(personID)
+        {
+            EventID = eventID;
+            Seen = false;
+        }
+
+        public void ChangeSeenStatus(bool newStatus) => Seen = newStatus;
+    }
+}
