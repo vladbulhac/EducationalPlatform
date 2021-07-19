8cb82d7a7aea1f3ad6382a7f1ecd3c4fdc6dd05b
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 15:44:43 +0300

    added TypeExtensionMethods static class

---
+++ src/Validation/DataValidation/TypeExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataValidation
+{
+    public static class TypeExtensionMethods
+    {
+        /// <summary>
+        /// Searches for <paramref name="lookedUpGenericType"/> in the inherited classes by <paramref name="type"/>
+        /// </summary>
+        public static bool IsSubclassOfGeneric(this Type type, Type lookedUpGenericType)
+        {
+            while (type is not null && type != typeof(object))
+            {
+                if (type.IsGenericType && type.GetGenericTypeDefinition() == lookedUpGenericType)
+                    return true;
+
+                type = type.BaseType;
+            }
+            return false;
+        }
+    }
+}
