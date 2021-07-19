7175b5832d38477b007c21877847a4576fdbcf64
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 18:28:30 +0300

    moved DTOEducationalInstitutionByLocationQueryValidator to Application, renamed to GetAllEducationalInstitutionsByLocationQueryValidator

---
+++ src/Services/Edu/EducationalInstitution.Application/Queries/Validators/GetAllEducationalInstitutionsByLocationQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace EducationalInstitution.Application.Queries.Validators
+{
+    public class GetAllEducationalInstitutionsByLocationQueryValidator : AbstractValidator<GetAllEducationalInstitutionsByLocationQuery>
+    {
+        public GetAllEducationalInstitutionsByLocationQueryValidator()
+        {
+            CascadeMode = CascadeMode.Stop;
+            RuleFor(dto => dto.LocationID)
+                                .NotEmpty()
+                                    .WithMessage("{PropertyName} was empty or null!")
+                                .Matches(@"\b[a-fA-F0-9]{24}$")
+                                    .WithMessage("{PropertyName} contains characters that are not supported and/or the length is not exactly 24!");
+        }
+    }
+}
