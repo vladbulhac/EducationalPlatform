8f30e27a3cf1fa64a05c1b792367cd9a9efcbdfa
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 17:55:55 +0300

    moved DTOEducationalInstitutionDeleteCommandValidator to Application, renamed to DisableEducationalInstitutionCommandValidator

---
+++ src/Services/Edu/EducationalInstitution.Application/Commands/Validators/DisableEducationalInstitutionCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace EducationalInstitution.Application.Commands.Validators
+{
+    public class DisableEducationalInstitutionCommandValidator : AbstractValidator<DisableEducationalInstitutionCommand>
+    {
+        public DisableEducationalInstitutionCommandValidator()
+        {
+            CascadeMode = CascadeMode.Stop;
+            RuleFor(dto => dto.EducationalInstitutionID)
+                              .NotEmpty()
+                                .WithMessage("{PropertyName} was empty or null!");
+        }
+    }
+}
