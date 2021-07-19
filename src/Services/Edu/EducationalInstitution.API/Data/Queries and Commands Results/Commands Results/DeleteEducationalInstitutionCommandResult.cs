852278073b5638f7ce31e61160fa06ded6c736c9
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 18:35:20 +0300

    moved to Application

--- src/Services/Edu/EducationalInstitution.API/Data/Queries and Commands Results/Commands Results/DeleteEducationalInstitutionCommandResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace EducationalInstitutionAPI.Data.Queries_and_Commands_Results.Commands_Results
-{
-    public record DeleteEducationalInstitutionCommandResult
-    {
-        public DateTime DateForPermanentDeletion { get; init; }
-    }
-}
--- src/Services/Edu/EducationalInstitution.API/Data/Queries and Commands Results/Commands Results/EducationalInstitutionCommandResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace EducationalInstitutionAPI.Data.Queries_and_Commands_Results.Commands_Results
-{
-    public record EducationalInstitutionCommandResult
-    {
-        public Guid EducationalInstitutionID { get; init; }
-    }
-}
