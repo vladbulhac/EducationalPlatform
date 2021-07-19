6bf58dbf5b048943870b74940232866b85e40ad4
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 16:34:54 +0300

    moved to Infrastructure, changed entity type configurations calls  by calling ModelBuilder's ApplyConfiguration method

---
+++ src/Services/Edu/EducationalInstitution.Infrastructure/DataContext.cs
@@ -0,0 +1,34 @@
+﻿using EducationalInstitution.Domain.Models;
+using EducationalInstitution.Infrastructure.Entities_Type_Configuration;
+using Microsoft.EntityFrameworkCore;
+
+using Domain = EducationalInstitution.Domain.Models.Aggregates;
+
+namespace EducationalInstitution.Infrastructure
+{
+    /// <summary>
+    /// Contains the configuration of each entity
+    /// </summary>
+    public class DataContext : DbContext
+    {
+        public virtual DbSet<Domain::EducationalInstitution> EducationalInstitutions { get; set; }
+        public virtual DbSet<EducationalInstitutionBuilding> Buildings { get; set; }
+        public virtual DbSet<EducationalInstitutionAdmin> Admins { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            /*if (Database.ProviderName != "Microsoft.EntityFrameworkCore.InMemory")
+                Database.Migrate();*/
+        }
+
+        public DataContext()
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EducationalInstitutionBuildingEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new EducationalInstitutionEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new EducationalInstitutionAdminEntityTypeConfiguration());
+        }
+    }
+}
