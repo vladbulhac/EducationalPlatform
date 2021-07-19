c203bec04806d392c9e3faa3c63184958304b82a
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 16:01:40 +0300

    added GetEventDetails

---
+++ src/Services/Notification/Notification.Infrastructure/Repositories/Results/GetEventDetails.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Notification.Infrastructure.Repositories.Results
+{
+    public record GetEventDetails
+    {
+        public DateTime TimeIssued { get; init; }
+        public string Message { get; init; }
+        public string Url { get; init; }
+    }
+}
