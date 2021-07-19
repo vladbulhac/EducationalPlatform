350d97b85502c480a1a4e4c67dfad33e5c51986b
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 16:38:31 +0300

    moved EducationalInstitutionAdminEntityTypeConfiguration to Infrastructure, configured Id property to have ColumnName: EducationalInstitutionID, configured OwnsOne Access

---
+++ src/Services/Edu/EducationalInstitution.Infrastructure/Entities Type Configuration/EducationalInstitutionAdminEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using EducationalInstitution.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EducationalInstitution.Infrastructure.Entities_Type_Configuration
+{
+    public class EducationalInstitutionAdminEntityTypeConfiguration : IEntityTypeConfiguration<EducationalInstitutionAdmin>
+    {
+        public void Configure(EntityTypeBuilder<EducationalInstitutionAdmin> builder)
+        {
+            builder.HasKey(eia => new { eia.AdminID, eia.Id });
+
+            builder.Property(eib => eib.Id).HasColumnName("EducationalInstitutionID");
+
+            builder.OwnsOne(eia => eia.Access, exp =>
+            {
+                exp.HasIndex(eia => new { eia.IsDisabled });
+                exp.Property(a => a.IsDisabled).HasColumnName("IsDisabled");
+                exp.Property(a => a.DateForPermanentDeletion).HasColumnName("DateForPermanentDeletion");
+            });
+        }
+    }
+}
