428df7cf29bcbe196a9d19c86bda3b6e87dcd5ca
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 17:50:56 +0300

    moved EducationalInstitutionCommandResult to Application, renamed to CreateEducationalInstitutionCommandResult

---
+++ src/Services/Edu/EducationalInstitution.Application/Commands/Results/CreateEducationalInstitutionCommandResult.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EducationalInstitution.Application.Commands.Results
+{
+    public record CreateEducationalInstitutionCommandResult
+    {
+        public Guid EducationalInstitutionID { get; init; }
+    }
+}
