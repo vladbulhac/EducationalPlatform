a91b271ebb4656730a5ea53a357069f78e279af7
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 18:24:53 +0300

    moved DTOEducationalInstitutionByIDQuery to Application, renamed to GetEducationalInstitutionByIDQuery

---
+++ src/Services/Edu/EducationalInstitution.Application/Queries/GetEducationalInstitutionByIDQuery.cs
@@ -0,0 +1,11 @@
+﻿using EducationalInstitution.Infrastructure.Repositories.Query_Repository.Results;
+using MediatR;
+using System;
+
+namespace EducationalInstitution.Application.Queries
+{
+    public class GetEducationalInstitutionByIDQuery : IRequest<Response<GetEducationalInstitutionByIDQueryResult>>
+    {
+        public Guid EducationalInstitutionID { get; init; }
+    }
+}
