addf495b891124bf49740e596b13722b44b3d6c1
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 17:48:51 +0300

    moved DTOEducationalInstitutionLocationUpdateCommand to Application, renamed to UpdateEducationalInstitutionLocationCommand

---
+++ src/Services/Edu/EducationalInstitution.Application/Commands/UpdateEducationalInstitutionLocationCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace EducationalInstitution.Application.Commands
+{
+    public class UpdateEducationalInstitutionLocationCommand : IRequest<Response>
+    {
+        public Guid EducationalInstitutionID { get; init; }
+
+        public bool UpdateLocation { get; init; }
+        public string LocationID { get; init; }
+
+        public bool UpdateBuildings { get; init; }
+        public ICollection<string> AddBuildingsIDs { get; init; }
+        public ICollection<string> RemoveBuildingsIDs { get; init; }
+    }
+}
