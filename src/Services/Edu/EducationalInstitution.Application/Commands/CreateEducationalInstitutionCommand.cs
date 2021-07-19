62c7da5154e0a514dfba9da6b7d30be24f8c66ec
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 17:44:09 +0300

    moved DTOEducationalInstitutionCreateCommand to Application, renamed to CreateEducationalInstitutionCommand

---
+++ src/Services/Edu/EducationalInstitution.Application/Commands/CreateEducationalInstitutionCommand.cs
@@ -0,0 +1,21 @@
+﻿using EducationalInstitution.Application.Commands.Results;
+using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace EducationalInstitution.Application.Commands
+{
+    public class CreateEducationalInstitutionCommand : IRequest<Response<CreateEducationalInstitutionCommandResult>>
+    {
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public string LocationID { get; init; }
+        public ICollection<string> BuildingsIDs { get; init; }
+        public ICollection<Guid> AdminsIDs { get; init; }
+
+        /// <remarks>
+        /// <i>If an Educational Institution is created without a parent then this field will be equal to the default value of Guid: 00000000-0000-0000-0000-000000000000</i>
+        /// </remarks>
+        public Guid ParentInstitutionID { get; init; }
+    }
+}
