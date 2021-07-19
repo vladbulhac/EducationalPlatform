6971eb3ba91e7de30090bd36794536d05669f593
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 17:28:37 +0300

    moved EducationalInstitutionAdmin to Domain, extends Entity

---
+++ src/Services/Edu/EducationalInstitution.Domain/Models/EducationalInstitutionAdmin.cs
@@ -0,0 +1,19 @@
+﻿using EducationalInstitution.Domain.Building_Blocks;
+using System;
+using Model = EducationalInstitution.Domain.Models.Aggregates;
+
+namespace EducationalInstitution.Domain.Models
+{
+    public class EducationalInstitutionAdmin : Entity
+    {
+        public Guid AdminID { get; init; }
+        public Model::EducationalInstitution EducationalInstitution { get; init; }
+
+        public EducationalInstitutionAdmin()
+        {
+        }
+
+        public EducationalInstitutionAdmin(Guid adminID, Guid educationalInstitutionID) : base(educationalInstitutionID)
+                => AdminID = adminID == default ? throw new ArgumentException($"{nameof(adminID)} doesn't have a valid value!") : adminID;
+    }
+}
