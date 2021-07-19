63877c5682c581a814fad78310f65903043879c4
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 15:51:09 +0300

    moved NotificationContext to Infrastructure

---
+++ src/Services/Notification/Notification.Infrastructure/NotificationContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notification.Domain.Models;
+using Notification.Domain.Models.Aggregates;
+using Notification.Infrastructure.Entities_Type_Configuration;
+
+namespace Notification.Infrastructure
+{
+    public class NotificationContext : DbContext
+    {
+        public virtual DbSet<Event> Events { get; set; }
+        public virtual DbSet<Recipient> Recipients { get; set; }
+
+        public NotificationContext(DbContextOptions<NotificationContext> options) : base(options)
+        {
+            if (Database.ProviderName != "Microsoft.EntityFrameworkCore.InMemory")
+                Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EventEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new RecipientEntityTypeConfiguration());
+        }
+    }
+}
