21b1cd09dd9204a42fd33297b9e8af222274cd9d
Vlad Bulhac <vladutbulhac@gmail.com>
2021-07-19 17:25:51 +0300

    added ValueObject

---
+++ src/Services/Edu/EducationalInstitution.Domain/Building Blocks/ValueObject.cs
@@ -0,0 +1,7 @@
+﻿namespace EducationalInstitution.Domain.Building_Blocks
+{
+    /// <summary>
+    /// Represents an immutable object that is defined by its attributes, it does not have an identity.
+    /// </summary>
+    public record ValueObject();
+}
