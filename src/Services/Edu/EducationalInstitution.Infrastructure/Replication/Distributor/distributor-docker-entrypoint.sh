3f8b8018cbabdae3ec837f26f0fc510f4b591def
Vlad Bulhac <vladutbulhac@gmail.com>
2021-08-04 20:04:00 +0300

    added distributor-docker-entrypoint.sh

---
+++ src/Services/Edu/EducationalInstitution.Infrastructure/Replication/Distributor/distributor-docker-entrypoint.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+
+# https://github.com/vishnubob/wait-for-it
+# run SQL Server and wait-for.sh scripts
+# -> wait for Distributor database to accept connections (to be ready)
+# -> wait for Publisher database to be ready
+# -> after publisher database is ready run /distributor/distributor-execute-scripts.sh
+./wait-for.sh 127.0.0.1:1433 -t 0 -- ./wait-for.sh edu-publisher:1433 -t 0 -- /distributor/distributor-execute-scripts.sh & /opt/mssql/bin/sqlservr
