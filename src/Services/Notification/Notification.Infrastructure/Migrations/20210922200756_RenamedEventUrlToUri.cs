7b54d4601b320d0239b193398c878e29b269c1db
Vlad Bulhac <vladutbulhac@gmail.com>
2021-09-22 23:53:24 +0300

    added new migration RenamedEventUrlToUri

---
+++ src/Services/Notification/Notification.Infrastructure/Migrations/20210922200756_RenamedEventUrlToUri.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Notification.Infrastructure.Migrations
+{
+    public partial class RenamedEventUrlToUri : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Url",
+                table: "Events",
+                newName: "Uri");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Events_Name_Message_Url",
+                table: "Events",
+                newName: "IX_Events_Name_Message_Uri");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Uri",
+                table: "Events",
+                newName: "Url");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Events_Name_Message_Uri",
+                table: "Events",
+                newName: "IX_Events_Name_Message_Url");
+        }
+    }
+}
