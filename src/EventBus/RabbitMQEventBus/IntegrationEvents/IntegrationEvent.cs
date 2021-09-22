1eb9e74329b26d319dd7f9b651e7bccb5359c203
Vlad Bulhac <vladutbulhac@gmail.com>
2021-09-22 23:59:22 +0300

    added EventId property and renamed Url property to Uri

--- src/EventBus/RabbitMQEventBus/IntegrationEvents/IntegrationEvent.cs
+++ src/EventBus/RabbitMQEventBus/IntegrationEvents/IntegrationEvent.cs
@@ -4,12 +4,17 @@ namespace RabbitMQEventBus.IntegrationEvents
 {
     public record IntegrationEvent
     {
+        public string EventId { get; init; }
         public DateTime TimeIssued { get; private set; }
         public EventTrigger TriggeredBy { get; init; }
-        public string Url { get; init; }
+        public string Uri { get; init; }
         public string Message { get; init; }
 
-        public IntegrationEvent() => TimeIssued = DateTime.UtcNow;
+        public IntegrationEvent()
+        {
+            EventId = Guid.NewGuid().ToString();
+            TimeIssued = DateTime.UtcNow;
+        }
     }
 
     public record EventTrigger
