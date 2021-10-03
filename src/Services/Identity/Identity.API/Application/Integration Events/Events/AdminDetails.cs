d4e7fd052e984daaa8466bddb9831826ad12c8b9
Vlad Bulhac <vladutbulhac@gmail.com>
2021-10-03 21:00:53 +0300

    added AdminDetails

---
+++ src/Services/Identity/Identity.API/Application/Integration Events/Events/AdminDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.API.Application.Integration_Events.Events
+{
+    public record AdminDetailsForIntegrationEvent : AdminDetails
+    {
+        public string DetailedMessage { get; init; }
+    }
+
+    public record AdminDetails
+    {
+        public string Identity { get; init; }
+        public ICollection<string> Permissions { get; init; }
+    }
+}
