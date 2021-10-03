1ea9301fb6fac091293cdf5b00fa55a9ab146267
Vlad Bulhac <vladutbulhac@gmail.com>
2021-10-03 20:45:42 +0300

    changed inherited class to StringEntity, added Guid EducationalInstitutionId

--- src/Services/Edu/EducationalInstitution.Domain/Models/EducationalInstitutionBuilding.cs
+++ src/Services/Edu/EducationalInstitution.Domain/Models/EducationalInstitutionBuilding.cs
@@ -4,16 +4,16 @@ using Model = EducationalInstitution.Domain.Models.Aggregates;
 
 namespace EducationalInstitution.Domain.Models
 {
-    public class EducationalInstitutionBuilding : Entity
+    public class EducationalInstitutionBuilding : StringEntity
     {
         public Model::EducationalInstitution EducationalInstitution { get; init; }
-        public string BuildingID { get; init; }
+        public Guid EducationalInstitutionId { get; init; }
 
         public EducationalInstitutionBuilding()
         {
         }
 
-        public EducationalInstitutionBuilding(string buildingID, Guid educationalInstitutionID) : base(educationalInstitutionID)
-                => BuildingID = string.IsNullOrEmpty(buildingID) ? throw new ArgumentNullException(nameof(buildingID)) : buildingID;
+        public EducationalInstitutionBuilding(string buildingId, Guid educationalInstitutionID) : base(buildingId)
+                => EducationalInstitutionId = educationalInstitutionID;
     }
 }
