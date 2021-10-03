a5781f88ebd465a7708ab6fb0f92f0f4340c8b86
Vlad Bulhac <vladutbulhac@gmail.com>
2021-10-03 20:39:58 +0300

    added UpdatedAdminsPermissionsIntegrationEvent

---
+++ src/Services/Edu/EducationalInstitution.Application/Integration Events/UpdatedAdminsPermissionsIntegrationEvent.cs
@@ -0,0 +1,15 @@
+﻿using RabbitMQEventBus.IntegrationEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationalInstitution.Application.Integration_Events
+{
+    public record UpdatedAdminsPermissionsIntegrationEvent : IntegrationEvent
+    {
+        public Guid EducationalInstitutionId { get; init; }
+        public ICollection<AdminDetailsForIntegrationEvent> UpdatedAdmins { get; init; }
+    }
+}
