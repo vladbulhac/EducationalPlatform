2f6330685fbf19f452b5ecd89fa30e93779d7791
Vlad Bulhac <vladutbulhac@gmail.com>
2021-10-03 20:40:30 +0300

    added AdminDetails

---
+++ src/Services/Edu/EducationalInstitution.Application/AdminDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationalInstitution.Application
+{
+    public record AdminDetails
+    {
+        public string Identity { get; init; }
+        public ICollection<string> Permissions { get; init; }
+    }
+}
