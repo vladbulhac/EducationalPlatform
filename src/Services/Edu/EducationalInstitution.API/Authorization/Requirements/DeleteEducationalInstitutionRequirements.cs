ea1b86b37fdd1dc40c55ac35200eda5eba4b4d54
Vlad Bulhac <vladutbulhac@gmail.com>
2021-10-03 19:44:43 +0300

    added DeleteEducationalInstitutionRequirements

---
+++ src/Services/Edu/EducationalInstitution.API/Authorization/Requirements/DeleteEducationalInstitutionRequirements.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationalInstitutionAPI.Authorization.Requirements
+{
+    public class DeleteEducationalInstitutionRequirements : IAuthorizationRequirement
+    {
+        public ResourcePolicy Policy { get; init; }
+
+        public DeleteEducationalInstitutionRequirements()
+        {
+            Policy = new DeleteEducationalInstitutionPolicy();
+        }
+
+        public DeleteEducationalInstitutionRequirements(ResourcePolicy policy)
+        {
+            Policy = policy;
+        }
+    }
+}
