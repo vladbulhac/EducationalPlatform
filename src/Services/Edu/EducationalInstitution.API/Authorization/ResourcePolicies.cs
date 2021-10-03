6d8cc042f6eb1c7425d95df13cbbf4e12a5e3783
Vlad Bulhac <vladutbulhac@gmail.com>
2021-10-03 19:45:17 +0300

    added ResourcePolicies

---
+++ src/Services/Edu/EducationalInstitution.API/Authorization/ResourcePolicies.cs
@@ -0,0 +1,25 @@
+﻿namespace EducationalInstitutionAPI.Authorization
+{
+    /// <summary>
+    /// Defines a set of rules that must be abided by in order to access a resource
+    /// </summary>
+    public record ResourcePolicy
+    {
+        public readonly ResourceAccessConstraints[] constraints;
+
+        public ResourcePolicy(ResourceAccessConstraints[] constraints) => this.constraints = constraints;
+    }
+
+    public record DeleteEducationalInstitutionPolicy : ResourcePolicy
+    {
+        public DeleteEducationalInstitutionPolicy() : base(new ResourceAccessConstraints[1] { ResourceOwnerPermissionsConstraints() })
+        { }
+        private static ResourceAccessConstraints ResourceOwnerPermissionsConstraints() => new(
+                                                                    claims: new(2)
+                                                                    {
+                                                                        { "user.educational_institution.all", null },
+                                                                        { "user.educational_institution.delete", null }
+                                                                    },
+                                                                    minimumClaimsNeeded: 1);
+    }
+}
