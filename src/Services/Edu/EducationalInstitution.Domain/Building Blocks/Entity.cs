e00168f8cc4fb32bb7e93c7b3b9c3fb3d511f27d
Vlad Bulhac <vladutbulhac@gmail.com>
2021-10-03 20:41:38 +0300

    converted Entity class to abstract generic class, added GetDeletionDate method, added GuidEntity and StringEntity classes

--- src/Services/Edu/EducationalInstitution.Domain/Building Blocks/Entity.cs
+++ src/Services/Edu/EducationalInstitution.Domain/Building Blocks/Entity.cs
@@ -6,22 +6,11 @@ namespace EducationalInstitution.Domain.Building_Blocks
     /// <summary>
     /// Represents an object that is defined by its identity and not by its attributes.
     /// </summary>
-    public abstract class Entity
+    public abstract class Entity<TKey>
     {
-        public Guid Id { get; init; }
+        public TKey Id { get; init; }
 
         public Access Access { get; protected set; }
-
-        protected Entity()
-        { }
-
-        protected Entity(Guid? id)
-        {
-            if (id.HasValue && id == Guid.Empty) throw new ArgumentException($"{nameof(id)} doesn't have a valid value!");
-
-            Id = id ?? Guid.NewGuid();
-            Access = new();
-        }
 
         public virtual void ScheduleForDeletion(double daysUntilDeletion = 30)
         {
@@ -38,5 +27,48 @@ namespace EducationalInstitution.Domain.Building_Blocks
         }
 
         public void ClearDeletionDate() => Access = new() { IsDisabled = false, DateForPermanentDeletion = null };
+
+        public DateTime? GetDeletionDate()
+        {
+            if (!Access.IsDisabled) return null;
+
+            return Access.DateForPermanentDeletion;
+        }
+    }
+
+    /// <summary>
+    /// <para>Id type is <see cref="Guid"/></para>
+    /// <inheritdoc cref="Entity{TKey}"/>
+    /// </summary>
+    public abstract class GuidEntity : Entity<Guid>
+    {
+        protected GuidEntity()
+        {
+        }
+
+        protected GuidEntity(Guid? id)
+        {
+            if (id.HasValue && id == Guid.Empty) throw new ArgumentException($"{nameof(id)} doesn't have a valid value!");
+
+            Id = id ?? Guid.NewGuid();
+            Access = new();
+        }
+    }
+
+    /// <summary>
+    /// <para>Id type is <see cref="string"/></para>
+    /// <inheritdoc cref="Entity{TKey}"/>
+    /// </summary>
+    public abstract class StringEntity : Entity<string>
+    {
+        protected StringEntity()
+        {
+        }
+
+        protected StringEntity(string id)
+        {
+            Id = id ?? Guid.NewGuid().ToString();
+            Access = new();
+        }
     }
 }
