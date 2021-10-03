897826339c89dc2f0a5b5464e1730b75bb49f86e
Vlad Bulhac <vladutbulhac@gmail.com>
2021-10-03 20:40:15 +0300

    added UserPermissions

---
+++ src/Services/Edu/EducationalInstitution.Application/Permissions/UserPermissions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationalInstitution.Application.Permissions
+{
+    public record UserPermissions
+    {
+        public const string All = "user.educational_institution.all";
+        public const string UpdateDetails = "user.educational_institution.update_details";
+        public const string ChangeAdministrators = "user.educational_institution.change_administrators";
+        public const string Delete = "user.educational_institution.delete";
+    }
+}
