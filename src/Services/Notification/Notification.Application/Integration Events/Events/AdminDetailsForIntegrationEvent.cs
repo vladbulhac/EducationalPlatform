7fd40be1917da97b303dfb666e572387a8338b84
Vlad Bulhac <vladutbulhac@gmail.com>
2021-10-03 22:53:11 +0300

    added AdminDetailsForIntegrationEvent

---
+++ src/Services/Notification/Notification.Application/Integration Events/Events/AdminDetailsForIntegrationEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationalInstitution.Application.Integration_Events
+{
+    public record AdminDetailsForIntegrationEvent : AdminDetails
+    {
+        public string DetailedMessage { get; init; }
+    }
+    public record AdminDetails
+    {
+        public string Identity { get; init; }
+        public ICollection<string> Permissions { get; init; }
+    }
+}
