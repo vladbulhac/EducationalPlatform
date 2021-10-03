cb83d8d4dc89c9ddc3d662733ded6caaee3f6c77
Vlad Bulhac <vladutbulhac@gmail.com>
2021-10-03 18:47:18 +0300

    added AdminDetails

---
+++ src/APIGateway/Aggregator/Models/DTOs/EducationalInstitutionDTOs/AdminDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aggregator.Models.DTOs.EducationalInstitutionDTOs
+{
+    public record AdminDetails
+    {
+        public string Identity { get; init; }
+        public ICollection<string> Permissions { get; init; }
+    }
+}
