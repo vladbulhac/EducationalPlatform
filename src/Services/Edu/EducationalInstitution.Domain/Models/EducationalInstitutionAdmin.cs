9449729d644dd8e8b3ac84a2fa830c0b9d6b67d9
Vlad Bulhac <vladutbulhac@gmail.com>
2021-10-03 20:45:04 +0300

    changed inherited class to StringEntity, added collection of permissions, added new constructor, added RevokePermissions and GrantPermissions methods, added Guid EducationalInstitutionId

--- src/Services/Edu/EducationalInstitution.Domain/Models/EducationalInstitutionAdmin.cs
+++ src/Services/Edu/EducationalInstitution.Domain/Models/EducationalInstitutionAdmin.cs
@@ -1,19 +1,39 @@
 ﻿using EducationalInstitution.Domain.Building_Blocks;
 using System;
+using System.Collections.Generic;
 using Model = EducationalInstitution.Domain.Models.Aggregates;
 
 namespace EducationalInstitution.Domain.Models
 {
-    public class EducationalInstitutionAdmin : Entity
+    public class EducationalInstitutionAdmin : StringEntity
     {
-        public string AdminId { get; init; }
+        public Guid EducationalInstitutionId { get; init; }
         public Model::EducationalInstitution EducationalInstitution { get; init; }
+        public ICollection<string> Permissions { get; private set; }
 
         public EducationalInstitutionAdmin()
         {
         }
 
-        public EducationalInstitutionAdmin(string adminID, Guid educationalInstitutionID) : base(educationalInstitutionID)
-                => AdminId = adminID ?? throw new ArgumentException($"{nameof(adminID)} doesn't have a valid value!");
+        public EducationalInstitutionAdmin(string adminId, Guid educationalInstitutionId, ICollection<string> permissions) : base(adminId)
+        {
+            EducationalInstitutionId = educationalInstitutionId;
+
+            if (permissions is null) throw new ArgumentNullException($"{nameof(permissions)} is null or empty!");
+
+            Permissions = new HashSet<string>(permissions);
+        }
+
+        public void RevokePermissions(ICollection<string> revokedPermissions)
+        {
+            foreach (var permission in revokedPermissions)
+                Permissions.Remove(permission);
+        }
+
+        public void GrantPermissions(ICollection<string> grantedPermissions)
+        {
+            foreach (var permission in grantedPermissions)
+                Permissions.Add(permission);
+        }
     }
 }
