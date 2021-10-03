e425d664f492d6735281bb6cb8547e62e750b49f
Vlad Bulhac <vladutbulhac@gmail.com>
2021-10-03 21:01:18 +0300

    added AssignedAdminsToEducationalInstitutionIntegrationEvent

---
+++ src/Services/Identity/Identity.API/Application/Integration Events/Events/AssignedAdminsToEducationalInstitutionIntegrationEvent.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQEventBus.IntegrationEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.API.Application.Integration_Events.Events
+{
+    public record AssignedAdminsToEducationalInstitutionIntegrationEvent : IntegrationEvent
+    {
+        public Guid EducationalInstitutionId { get; init; }
+        public ICollection<AdminDetailsForIntegrationEvent> NewAdmins { get; init; }
+    }
+}
