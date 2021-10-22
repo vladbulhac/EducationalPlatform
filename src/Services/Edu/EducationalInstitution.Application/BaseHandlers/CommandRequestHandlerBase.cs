16d3e9100e8da115fbdd52fcfc233d16cddf498e
Vlad Bulhac <vladutbulhac@gmail.com>
2021-10-22 20:27:38 +0300

    added CommandRequestHandlerBase

---
+++ src/Services/Edu/EducationalInstitution.Application/BaseHandlers/CommandRequestHandlerBase.cs
@@ -0,0 +1,36 @@
+﻿using EducationalInstitution.Infrastructure.Repositories.Command_Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
+using RabbitMQEventBus.IntegrationEvents;
+using RabbitMQEventBus.Transactional_Outbox.Services.Outbox_Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EducationalInstitution.Application.BaseHandlers
+{
+    public abstract class CommandRequestHandlerBase<THandler, TRequest, TResponse> : HandlerBase<THandler>, IRequestHandler<TRequest, TResponse> where TRequest : IRequest<TResponse> where THandler : class
+    {
+        protected CommandRequestHandlerBase(ILogger<THandler> logger) : base(logger)
+        {
+        }
+
+        public abstract Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken);
+
+        protected abstract Task<TResponse> TransactionOperations(IDbContextTransaction transaction, IIntegrationEventOutboxService eventOutboxService, TRequest request);
+
+        protected async Task PublishIntegrationEventsAsync(IDbContextTransaction transaction, IIntegrationEventOutboxService eventOutboxService, List<IntegrationEvent> @events)
+        {
+            await eventOutboxService.SaveMultipleEventsToDatabaseAsync(@events, transaction);
+        }
+
+        protected async Task PublishIntegrationEventAsync(IDbContextTransaction transaction, IIntegrationEventOutboxService eventOutboxService, IntegrationEvent @event)
+        {
+            await eventOutboxService.SaveEventToDatabaseAsync(@event, transaction);
+        }
+    }
+}
