0ae560b8a916db44ee4c4838b9faa33cb42d3ed6
Vlad Bulhac <vladutbulhac@gmail.com>
2021-10-22 20:35:18 +0300

    added ExecuteTransactionAsync method, updated IDisposable implementation

--- src/Services/Edu/EducationalInstitution.Infrastructure/Unit of Work/Command Unit of Work/IUnitOfWorkForCommands.cs
+++ src/Services/Edu/EducationalInstitution.Infrastructure/Unit of Work/Command Unit of Work/IUnitOfWorkForCommands.cs
@@ -1,4 +1,8 @@
 ﻿using EducationalInstitution.Infrastructure.Repositories.Command_Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore.Storage;
+using RabbitMQEventBus.Transactional_Outbox.Infrastructure;
+using RabbitMQEventBus.Transactional_Outbox.Services.Outbox_Services;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,6 +13,8 @@ namespace EducationalInstitution.Infrastructure.Unit_of_Work.Command_Unit_of_Wor
     {
         /// <returns>Returns an instance of a Repository class that implements <see cref="IEducationalInstitutionCommandRepository"/></returns>
         public IEducationalInstitutionCommandRepository UsingEducationalInstitutionCommandRepository();
+
+        public Task<TResponse> ExecuteTransactionAsync<TRequest, TResponse>(Func<IDbContextTransaction, IIntegrationEventOutboxService, TRequest, Task<TResponse>> transactionOperations, TRequest request);
 
         public Task SaveChangesAsync(CancellationToken cancellationToken = default);
     }
--- src/Services/Edu/EducationalInstitution.Infrastructure/Unit of Work/Command Unit of Work/UnitOfWorkForCommands.cs
+++ src/Services/Edu/EducationalInstitution.Infrastructure/Unit of Work/Command Unit of Work/UnitOfWorkForCommands.cs
@@ -1,5 +1,12 @@
 ﻿using EducationalInstitution.Infrastructure.Repositories.Command_Repository;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
+using RabbitMQEventBus.Transactional_Outbox.Infrastructure;
+using RabbitMQEventBus.Transactional_Outbox.Services.MessageRelay;
+using RabbitMQEventBus.Transactional_Outbox.Services.Outbox_Services;
+using RabbitMQEventBus.Transactional_Outbox.Services.Transaction;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,11 +16,17 @@ namespace EducationalInstitution.Infrastructure.Unit_of_Work.Command_Unit_of_Wor
     public class UnitOfWorkForCommands : IUnitOfWorkForCommands
     {
         private bool disposed;
+
         private readonly DataContext context;
+        private readonly ITransactionService transactionService;
 
         public IEducationalInstitutionCommandRepository EducationalInstitutionRepository { get; private set; }
 
-        public UnitOfWorkForCommands(DbContextOptions<DataContext> options) => context = new(options);
+        public UnitOfWorkForCommands(ILoggerFactory loggerFactory, IMessageRelayService messageRelay, DbContextOptions<DataContext> options)
+        {
+            context = new(options);
+            transactionService = new TransactionService(context, messageRelay, loggerFactory);
+        }
 
         public IEducationalInstitutionCommandRepository UsingEducationalInstitutionCommandRepository()
         {
@@ -23,23 +36,31 @@ namespace EducationalInstitution.Infrastructure.Unit_of_Work.Command_Unit_of_Wor
             return EducationalInstitutionRepository;
         }
 
+        public async Task<TResponse> ExecuteTransactionAsync<TRequest, TResponse>(Func<IDbContextTransaction, IIntegrationEventOutboxService, TRequest, Task<TResponse>> transactionOperations, TRequest request)
+        {
+            return await transactionService.ExecuteTransactionAsync(transactionOperations, request);
+        }
+
         public async Task SaveChangesAsync(CancellationToken cancellationToken = default) => await context.SaveChangesAsync(cancellationToken);
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
         protected virtual void Dispose(bool disposing)
         {
             if (!disposed)
             {
                 if (disposing)
+                {
                     context.Dispose();
+                    transactionService.Dispose();
+                }
             }
 
             disposed = true;
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
         }
     }
 }
