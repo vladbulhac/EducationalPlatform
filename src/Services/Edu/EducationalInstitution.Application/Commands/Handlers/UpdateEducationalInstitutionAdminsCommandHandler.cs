75bd7dc0afc175f7a7132e3c420e851beb698272
Vlad Bulhac <vladutbulhac@gmail.com>
2021-11-08 16:03:39 +0200

    inherits from CommandRequestHandlerBase, moved all transaction logic to TransactionOperations method, refactored BuildDetailedMessage method

--- src/Services/Edu/EducationalInstitution.Application/Commands/Handlers/UpdateEducationalInstitutionAdminsCommandHandler.cs
+++ src/Services/Edu/EducationalInstitution.Application/Commands/Handlers/UpdateEducationalInstitutionAdminsCommandHandler.cs
@@ -3,10 +3,12 @@ using EducationalInstitution.Application.Integration_Events;
 using EducationalInstitution.Infrastructure.Repositories.Command_Repository;
 using EducationalInstitution.Infrastructure.Unit_of_Work.Command_Unit_of_Work;
 using MediatR;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using RabbitMQEventBus.Abstractions;
 using RabbitMQEventBus.IntegrationEvents;
+using RabbitMQEventBus.Transactional_Outbox.Services.Outbox_Services;
 using System;
 using System.Collections.Generic;
 using System.Net;
@@ -16,17 +18,16 @@ using System.Threading.Tasks;
 
 namespace EducationalInstitution.Application.Commands.Handlers
 {
-    public class UpdateEducationalInstitutionAdminsCommandHandler : HandlerBase<UpdateEducationalInstitutionAdminsCommandHandler>,
-                                                                    IRequestHandler<UpdateEducationalInstitutionAdminsCommand, Response>
+    public class UpdateEducationalInstitutionAdminsCommandHandler : CommandRequestHandlerBase<UpdateEducationalInstitutionAdminsCommandHandler,
+                                                                                              UpdateEducationalInstitutionAdminsCommand,
+                                                                                              Response>
     {
         private readonly IUnitOfWorkForCommands unitOfWork;
-        private readonly IEventBus eventBus;
 
         /// <inheritdoc cref="HandlerBase{THandler}.HandlerBase"/>
-        public UpdateEducationalInstitutionAdminsCommandHandler(IUnitOfWorkForCommands unitOfWork, IEventBus eventBus, ILogger<UpdateEducationalInstitutionAdminsCommandHandler> logger) : base(logger)
+        public UpdateEducationalInstitutionAdminsCommandHandler(IUnitOfWorkForCommands unitOfWork, ILogger<UpdateEducationalInstitutionAdminsCommandHandler> logger) : base(logger)
         {
             this.unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
-            this.eventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
         }
 
         /// <summary>
@@ -42,7 +43,7 @@ namespace EducationalInstitution.Application.Commands.Handlers
         /// </list>
         /// </returns>
         /// <exception cref="ArgumentNullException"/>
-        public async Task<Response> Handle(UpdateEducationalInstitutionAdminsCommand request, CancellationToken cancellationToken = default)
+        public override async Task<Response> Handle(UpdateEducationalInstitutionAdminsCommand request, CancellationToken cancellationToken = default)
         {
             if (request is null) throw new ArgumentNullException(nameof(request));
 
@@ -50,33 +51,17 @@ namespace EducationalInstitution.Application.Commands.Handlers
             {
                 using (unitOfWork)
                 {
-                    var educationalInstitution = await unitOfWork.UsingEducationalInstitutionCommandRepository()
-                                                                 .GetEducationalInstitutionIncludingAdminsAsync(request.EducationalInstitutionID, cancellationToken);
+                    var transactionResult = await unitOfWork.ExecuteTransactionAsync(TransactionOperations, request);
 
-                    if (educationalInstitution == default)
+                    if (transactionResult == default)
                         return new()
                         {
                             OperationStatus = false,
-                            StatusCode = HttpStatusCode.NotFound,
-                            Message = $"Educational Institution with the following ID: {request.EducationalInstitutionID} has not been found!"
+                            StatusCode = HttpStatusCode.InternalServerError,
+                            Message = "Could not successfully handle all the required operations for this request!"
                         };
 
-                    foreach (var admin in request.NewAdmins)
-                        educationalInstitution.CreateAndAddAdmin(admin.Identity, admin.Permissions);
-
-                    foreach (var admin in request.AdminsWithNewPermissions)
-                        educationalInstitution.GrantAdminPermissions(admin.Identity, admin.Permissions);
-
-                    foreach (var admin in request.AdminsWithRevokedPermissions)
-                        educationalInstitution.RevokeAdminPermissions(admin.Identity, admin.Permissions);
-
-                    await unitOfWork.SaveChangesAsync(cancellationToken);
-
-                    eventBus.PublishMultiple(PublishNotificationEventsForAdmins(request.NewAdmins,
-                                                                                request.AdminsWithRevokedPermissions,
-                                                                                request.AdminsWithNewPermissions,
-                                                                                request.EducationalInstitutionID),
-                                                                                publisherConfirms: false);
+                    return transactionResult;
                 }
             }
             catch (Exception e)
@@ -90,7 +75,38 @@ namespace EducationalInstitution.Application.Commands.Handlers
                     nameof(IEducationalInstitutionCommandRepository.GetEducationalInstitutionIncludingAdminsAsync),
                     e.Message);
             }
+        }
 
+        protected override async Task<Response> TransactionOperations(IDbContextTransaction transaction, IIntegrationEventOutboxService eventOutboxService, UpdateEducationalInstitutionAdminsCommand request)
+        {
+            var educationalInstitution = await unitOfWork.UsingEducationalInstitutionCommandRepository()
+                                                         .GetEducationalInstitutionIncludingAdminsAsync(request.EducationalInstitutionID);
+
+            if (educationalInstitution == default)
+                return new()
+                {
+                    OperationStatus = false,
+                    StatusCode = HttpStatusCode.NotFound,
+                    Message = $"Educational Institution with the following ID: {request.EducationalInstitutionID} has not been found!"
+                };
+
+            foreach (var admin in request.NewAdmins)
+                educationalInstitution.CreateAndAddAdmin(admin.Identity, admin.Permissions);
+
+            foreach (var admin in request.AdminsWithNewPermissions)
+                educationalInstitution.GrantAdminPermissions(admin.Identity, admin.Permissions);
+
+            foreach (var admin in request.AdminsWithRevokedPermissions)
+                educationalInstitution.RevokeAdminPermissions(admin.Identity, admin.Permissions);
+
+            await unitOfWork.SaveChangesAsync();
+
+            await PublishIntegrationEventsAsync(transaction,
+                                                eventOutboxService,
+                                                CreateNotificationEventsForAdmins(request.NewAdmins,
+                                                                                   request.AdminsWithRevokedPermissions,
+                                                                                   request.AdminsWithNewPermissions,
+                                                                                   request.EducationalInstitutionID));
             return new()
             {
                 Message = string.Empty,
@@ -99,18 +115,15 @@ namespace EducationalInstitution.Application.Commands.Handlers
             };
         }
 
-        private IEnumerable<IntegrationEvent> PublishNotificationEventsForAdmins(ICollection<AdminDetails> newAdmins, ICollection<AdminDetails> adminsWithRevokedPermissions, ICollection<AdminDetails> adminsWithNewPermissions, Guid educationalInstitutionID)
+        private IEnumerable<IntegrationEvent> CreateNotificationEventsForAdmins(ICollection<AdminDetails> newAdmins, ICollection<AdminDetails> adminsWithRevokedPermissions, ICollection<AdminDetails> adminsWithNewPermissions, Guid educationalInstitutionID)
         {
             if (newAdmins.Count > 0)
-            {
                 yield return new AssignedAdminsToEducationalInstitutionIntegrationEvent(BuildDetailedMessage(newAdmins, $"permissions has been granted to you for the Educational Institution accessible at [{educationalInstitutionID}]"),
                                                                                         educationalInstitutionID,
                                                                                         $"/edu/{educationalInstitutionID}",
                                                                                         "Add");
-            }
 
             if (adminsWithRevokedPermissions.Count > 0)
-            {
                 yield return new UpdatedAdminsPermissionsIntegrationEvent
                 {
                     Message = "Admin rights revoked for an Educational Institution!",
@@ -123,47 +136,59 @@ namespace EducationalInstitution.Application.Commands.Handlers
                         Action = "Delete"
                     }
                 };
-            }
 
-            yield return new UpdatedAdminsPermissionsIntegrationEvent
-            {
-                Message = "Admin rights updated for an Educational Institution!",
-                EducationalInstitutionId = educationalInstitutionID,
-                UpdatedAdmins = BuildDetailedMessage(adminsWithNewPermissions, $"permissions have been granted for the Educational Institution accessible at [{educationalInstitutionID}]"),
-                Uri = $"/edu/{educationalInstitutionID}",
-                TriggeredBy = new()
+            if (adminsWithNewPermissions.Count > 0)
+                yield return new UpdatedAdminsPermissionsIntegrationEvent
                 {
-                    ServiceName = this.GetType().Namespace.Split('.')[0],
-                    Action = "Add"
-                }
-            };
+                    Message = "Admin rights updated for an Educational Institution!",
+                    EducationalInstitutionId = educationalInstitutionID,
+                    UpdatedAdmins = BuildDetailedMessage(adminsWithNewPermissions, $"permissions have been granted for the Educational Institution accessible at [{educationalInstitutionID}]"),
+                    Uri = $"/edu/{educationalInstitutionID}",
+                    TriggeredBy = new()
+                    {
+                        ServiceName = this.GetType().Namespace.Split('.')[0],
+                        Action = "Add"
+                    }
+                };
         }
 
-        private AdminDetailsForIntegrationEvent[] BuildDetailedMessage(ICollection<AdminDetails> newAdmins, string appendedMessage)
+        /// <summary>
+        /// For each given admin will create an <see cref="AdminDetailsForIntegrationEvent"/>.
+        /// </summary>
+        /// <remarks>
+        /// The constructed detailed messaged will look like: 'Permission1 Permission2 Permission3... PermissionN appendMessage'.
+        /// </remarks>
+        private AdminDetailsForIntegrationEvent[] BuildDetailedMessage(ICollection<AdminDetails> newAdmins, string appendMessage)
         {
             var result = new AdminDetailsForIntegrationEvent[newAdmins.Count];
+            var messageBuilder = new StringBuilder();
+
             var index = 0;
             foreach (var admin in newAdmins)
             {
-                var permissions = new StringBuilder();
-
-                foreach (var permission in admin.Permissions)
-                    permissions.Append(permission.Split('.')[2])
-                               .Append(' ');
-
-                permissions.Append(appendedMessage);
+                ConstructMessageInBuilder(admin.Permissions, appendMessage, messageBuilder);
 
                 result[index] = new()
                 {
-                    DetailedMessage = permissions.ToString(),
+                    DetailedMessage = messageBuilder.ToString(),
                     Identity = admin.Identity,
                     Permissions = admin.Permissions
                 };
-
                 index++;
+
+                messageBuilder.Clear();
             }
 
             return result;
+        }
+
+        private void ConstructMessageInBuilder(ICollection<string> permissions, string appendMessage, StringBuilder messageBuilder)
+        {
+            foreach (var permission in permissions)
+                messageBuilder.Append(permission.Split('.')[2])
+                              .Append(' ');
+
+            messageBuilder.Append(appendMessage);
         }
     }
 }
