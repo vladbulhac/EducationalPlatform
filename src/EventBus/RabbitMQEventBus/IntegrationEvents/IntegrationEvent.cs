b697a01f05e9acd423b0fec0adef62873b2733f8
Vlad Bulhac <vladutbulhac@gmail.com>
2021-11-10 20:01:00 +0200

    dotnet6-migration: converted record type of EventTrigger to record struct

--- src/EventBus/RabbitMQEventBus/IntegrationEvents/IntegrationEvent.cs
+++ src/EventBus/RabbitMQEventBus/IntegrationEvents/IntegrationEvent.cs
@@ -1,25 +1,22 @@
-﻿using System;
+﻿namespace RabbitMQEventBus.IntegrationEvents;
 
-namespace RabbitMQEventBus.IntegrationEvents
+public record IntegrationEvent
 {
-    public record IntegrationEvent
-    {
-        public string EventId { get; init; }
-        public DateTime TimeIssued { get; private set; }
-        public EventTrigger TriggeredBy { get; init; }
-        public string Uri { get; init; }
-        public string Message { get; init; }
+    public string EventId { get; init; }
+    public DateTime TimeIssued { get; private set; }
+    public EventTrigger TriggeredBy { get; init; }
+    public string Uri { get; init; }
+    public string Message { get; init; }
 
-        public IntegrationEvent()
-        {
-            EventId = Guid.NewGuid().ToString();
-            TimeIssued = DateTime.UtcNow;
-        }
-    }
-
-    public record EventTrigger
+    public IntegrationEvent()
     {
-        public string Action { get; init; }
-        public string ServiceName { get; init; }
+        EventId = Guid.NewGuid().ToString();
+        TimeIssued = DateTime.UtcNow;
     }
+}
+
+public record struct EventTrigger
+{
+    public string Action { get; init; }
+    public string ServiceName { get; init; }
 }
