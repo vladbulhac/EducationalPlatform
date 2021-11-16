785ef05c5f8348ff6219992959db3887b132a053
Vlad Bulhac <vladutbulhac@gmail.com>
2021-11-16 18:50:12 +0200

    updated the tests due to the changes to UpdateEducationalInstitutionParentCommandHandler, added new tests

---
+++ src/Services/Edu/EducationalInstitution.API.UnitTests/Application Tests/Commands Tests/Handlers Tests/UpdateParent/UpdateEducationalInstitutionParentCommandHandlerTests.cs
@@ -0,0 +1,200 @@
+﻿using EducationalInstitution.API.Tests.Shared;
+using EducationalInstitution.Application;
+using EducationalInstitution.Application.Commands;
+using EducationalInstitution.Application.Commands.Handlers;
+using Microsoft.EntityFrameworkCore.Storage;
+using Moq;
+using RabbitMQEventBus.Transactional_Outbox.Services.Outbox_Services;
+using System.Net;
+using Xunit;
+
+namespace EducationalInstitution.API.UnitTests.Application_Tests.Commands_Tests.Handlers_Tests.UpdateParent;
+
+public class UpdateEducationalInstitutionParentCommandHandlerTests : IClassFixture<MockDependenciesHelper<UpdateEducationalInstitutionParentCommandHandler>>,
+                                                                     IClassFixture<TestDataFromJSONParser>
+{
+    private readonly MockDependenciesHelper<UpdateEducationalInstitutionParentCommandHandler> dependenciesHelper;
+    private readonly TestDataFromJSONParser testDataHelper;
+
+    /// <remarks>Called before each test</remarks>
+    public UpdateEducationalInstitutionParentCommandHandlerTests(MockDependenciesHelper<UpdateEducationalInstitutionParentCommandHandler> dependenciesHelper, TestDataFromJSONParser testDataHelper)
+    {
+        this.dependenciesHelper = dependenciesHelper;
+        this.testDataHelper = testDataHelper;
+    }
+
+    [Fact]
+    public async Task GivenAnUpdateEducationalInstitutionParentCommand_ShouldReturnHttpStatusCodeNoContent()
+    {
+        //Arrange
+        UpdateEducationalInstitutionParentCommand request = new()
+        {
+            EducationalInstitutionID = testDataHelper.EducationalInstitutions[0].Id,
+            ParentInstitutionID = testDataHelper.EducationalInstitutions[1].Id
+        };
+
+        SetupMockedDependencies();
+
+        var handler = new UpdateEducationalInstitutionParentCommandHandler(dependenciesHelper.mockUnitOfWorkCommand.Object,
+                                                                           dependenciesHelper.mockLogger.Object);
+
+        //Act
+        var result = await handler.Handle(request);
+
+        //Assert
+        Assert.Equal(HttpStatusCode.NoContent, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task GivenAnUpdateEducationalInstitutionParentCommand_ShouldReturnEmptyMessage()
+    {
+        //Arrange
+        UpdateEducationalInstitutionParentCommand request = new()
+        {
+            EducationalInstitutionID = testDataHelper.EducationalInstitutions[0].Id,
+            ParentInstitutionID = testDataHelper.EducationalInstitutions[1].Id
+        };
+
+        SetupMockedDependencies();
+
+        var handler = new UpdateEducationalInstitutionParentCommandHandler(dependenciesHelper.mockUnitOfWorkCommand.Object,
+                                                                           dependenciesHelper.mockLogger.Object);
+
+        //Act
+        var result = await handler.Handle(request);
+
+        //Assert
+        Assert.Empty(result.Message);
+    }
+
+    [Fact]
+    public async Task GivenAnUpdateEducationalInstitutionParentCommand_ShouldReturnOperationStatusTrue()
+    {
+        //Arrange
+        UpdateEducationalInstitutionParentCommand request = new()
+        {
+            EducationalInstitutionID = testDataHelper.EducationalInstitutions[0].Id,
+            ParentInstitutionID = testDataHelper.EducationalInstitutions[1].Id
+        };
+
+        SetupMockedDependencies();
+
+        var handler = new UpdateEducationalInstitutionParentCommandHandler(dependenciesHelper.mockUnitOfWorkCommand.Object,
+                                                                           dependenciesHelper.mockLogger.Object);
+
+        //Act
+        var result = await handler.Handle(request);
+
+        //Assert
+        Assert.True(result.OperationStatus);
+    }
+
+    private void SetupMockedDependencies()
+    {
+        dependenciesHelper.mockUnitOfWorkCommand.Setup(muowc => muowc.ExecuteTransactionAsync(It.IsAny<Func<IDbContextTransaction, IIntegrationEventOutboxService, UpdateEducationalInstitutionParentCommand, Task<Response>>>(),
+                                                                                              It.IsAny<UpdateEducationalInstitutionParentCommand>()))
+                                                .ReturnsAsync(new Response
+                                                {
+                                                    Message = string.Empty,
+                                                    OperationStatus = true,
+                                                    StatusCode = HttpStatusCode.NoContent
+                                                });
+    }
+
+    private void SetupMockedDependenciesToFailTheTransaction()
+    {
+        dependenciesHelper.mockUnitOfWorkCommand.Setup(muowc => muowc.ExecuteTransactionAsync(It.IsAny<Func<IDbContextTransaction, IIntegrationEventOutboxService, UpdateEducationalInstitutionParentCommand, Task<Response>>>(),
+                                                                                              It.IsAny<UpdateEducationalInstitutionParentCommand>()))
+                                                .ReturnsAsync((Response)default);
+    }
+
+    [Fact]
+    public async Task GivenAnUpdateEducationalInstitutionParentCommand_TheTransactionFails_ShouldReturnOperationStatusFalse()
+    {
+        //Arrange
+        UpdateEducationalInstitutionParentCommand request = new()
+        {
+            EducationalInstitutionID = testDataHelper.EducationalInstitutions[0].Id,
+            ParentInstitutionID = testDataHelper.EducationalInstitutions[1].Id
+        };
+
+        SetupMockedDependenciesToFailTheTransaction();
+
+        var handler = new UpdateEducationalInstitutionParentCommandHandler(dependenciesHelper.mockUnitOfWorkCommand.Object,
+                                                                           dependenciesHelper.mockLogger.Object);
+
+        //Act
+        var result = await handler.Handle(request);
+
+        //Assert
+        Assert.False(result.OperationStatus);
+    }
+
+    [Fact]
+    public async Task GivenAnUpdateEducationalInstitutionParentCommand_TheTransactionFails_ShouldReturnHttpStatusCodeInternalServerError()
+    {
+        //Arrange
+        UpdateEducationalInstitutionParentCommand request = new()
+        {
+            EducationalInstitutionID = testDataHelper.EducationalInstitutions[0].Id,
+            ParentInstitutionID = testDataHelper.EducationalInstitutions[1].Id
+        };
+
+        SetupMockedDependenciesToFailTheTransaction();
+
+        var handler = new UpdateEducationalInstitutionParentCommandHandler(dependenciesHelper.mockUnitOfWorkCommand.Object,
+                                                                           dependenciesHelper.mockLogger.Object);
+
+        //Act
+        var result = await handler.Handle(request);
+
+        //Assert
+        Assert.Equal(HttpStatusCode.InternalServerError, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task GivenAnUpdateEducationalInstitutionParentCommand_TheTransactionFails_ShouldReturnExpectedMessage()
+    {
+        //Arrange
+        UpdateEducationalInstitutionParentCommand request = new()
+        {
+            EducationalInstitutionID = testDataHelper.EducationalInstitutions[0].Id,
+            ParentInstitutionID = testDataHelper.EducationalInstitutions[1].Id
+        };
+
+        SetupMockedDependenciesToFailTheTransaction();
+
+        var handler = new UpdateEducationalInstitutionParentCommandHandler(dependenciesHelper.mockUnitOfWorkCommand.Object,
+                                                                           dependenciesHelper.mockLogger.Object);
+
+        //Act
+        var result = await handler.Handle(request);
+
+        //Assert
+        Assert.Equal("Could not successfully handle all the required operations for this request!", result.Message);
+    }
+
+    [Fact]
+    public async Task GivenANullArgumentRequestToTheRequestHandlerHandleMethod_ShouldThrowArgumentNullException()
+    {
+        //Arrange
+        UpdateEducationalInstitutionParentCommandHandler handler = new(dependenciesHelper.mockUnitOfWorkCommand.Object, dependenciesHelper.mockLogger.Object);
+
+        //Assert
+        await Assert.ThrowsAsync<ArgumentNullException>(() => handler.Handle(null));
+    }
+
+    [Fact]
+    public void GivenANullArgumentRepositoryToTheRequestHandlerConstructor_ShouldThrowArgumentNullException()
+    {
+        //Assert
+        Assert.Throws<ArgumentNullException>(() => new UpdateEducationalInstitutionParentCommandHandler(null, dependenciesHelper.mockLogger.Object));
+    }
+
+    [Fact]
+    public void GivenANullArgumentLoggerToTheRequestHandlerConstructor_ShouldThrowArgumentNullException()
+    {
+        //Assert
+        Assert.Throws<ArgumentNullException>(() => new UpdateEducationalInstitutionParentCommandHandler(dependenciesHelper.mockUnitOfWorkCommand.Object, null));
+    }
+}
---
+++ src/Services/Edu/EducationalInstitution.API.UnitTests/Application Tests/Commands Tests/Handlers Tests/UpdateParent/UpdateParentCommandHandlerBaseTests.cs
@@ -0,0 +1,225 @@
+﻿using EducationalInstitution.API.Tests.Shared;
+using EducationalInstitution.Application.Commands;
+using EducationalInstitution.Application.Commands.Handlers;
+using Moq;
+using System.Net;
+using Xunit;
+using Domain = EducationalInstitution.Domain.Models.Aggregates;
+
+namespace EducationalInstitution.API.UnitTests.Application_Tests.Commands_Tests.Handlers_Tests.UpdateParent
+{
+    public class UpdateParentCommandHandlerBaseTests : UpdateEducationalInstitutionParentCommandHandler, IClassFixture<MockDependenciesHelper<UpdateEducationalInstitutionParentCommandHandler>>,
+                                                                                                         IClassFixture<TestDataFromJSONParser>
+    {
+        private readonly MockDependenciesHelper<UpdateEducationalInstitutionParentCommandHandler> dependenciesHelper;
+        private readonly TestDataFromJSONParser testDataHelper;
+
+        /// <remarks>Called before each test</remarks>
+        public UpdateParentCommandHandlerBaseTests(MockDependenciesHelper<UpdateEducationalInstitutionParentCommandHandler> dependenciesHelper, TestDataFromJSONParser testDataHelper)
+        : base(dependenciesHelper.mockUnitOfWorkCommand.Object, dependenciesHelper.mockLogger.Object)
+        {
+            this.dependenciesHelper = dependenciesHelper;
+            this.testDataHelper = testDataHelper;
+        }
+
+        [Fact]
+        public async Task GivenAnUpdateEducationalInstitutionParentCommand_ToTransactionOperationsMethod_ShouldReturnAResponseThatIncludesAStatusCodeNoContentField()
+        {
+            //Arrange
+            UpdateEducationalInstitutionParentCommand request = new()
+            {
+                EducationalInstitutionID = testDataHelper.EducationalInstitutions[0].Id,
+                ParentInstitutionID = testDataHelper.EducationalInstitutions[1].Id
+            };
+
+            SetupMockedDependencies();
+
+            //Act
+            var result = await TransactionOperations(dependenciesHelper.mockTransaction.Object, dependenciesHelper.mockOutboxService.Object, request);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task GivenAnUpdateEducationalInstitutionParentCommand_ToTransactionOperationsMethod_ShouldReturnAResponseThatIncludesAnEmptyMessageField()
+        {
+            //Arrange
+            UpdateEducationalInstitutionParentCommand request = new()
+            {
+                EducationalInstitutionID = testDataHelper.EducationalInstitutions[0].Id,
+                ParentInstitutionID = testDataHelper.EducationalInstitutions[1].Id
+            };
+
+            SetupMockedDependencies();
+
+            //Act
+            var result = await TransactionOperations(dependenciesHelper.mockTransaction.Object, dependenciesHelper.mockOutboxService.Object, request);
+
+            //Assert
+            Assert.Empty(result.Message);
+        }
+
+        [Fact]
+        public async Task GivenAnUpdateEducationalInstitutionParentCommand_ToTransactionOperationsMethod_ShouldReturnAResponseThatIncludesATrueOperationStatusField()
+        {
+            //Arrange
+            UpdateEducationalInstitutionParentCommand request = new()
+            {
+                EducationalInstitutionID = testDataHelper.EducationalInstitutions[0].Id,
+                ParentInstitutionID = testDataHelper.EducationalInstitutions[1].Id
+            };
+
+            SetupMockedDependencies();
+
+            //Act
+            var result = await TransactionOperations(dependenciesHelper.mockTransaction.Object, dependenciesHelper.mockOutboxService.Object, request);
+
+            //Assert
+            Assert.True(result.OperationStatus);
+        }
+
+        private void SetupMockedDependencies()
+        {
+            dependenciesHelper.mockUnitOfWorkCommand.Setup(uok => uok.UsingEducationalInstitutionCommandRepository())
+                                                    .Returns(dependenciesHelper.mockEducationalInstitutionCommandRepository.Object);
+
+            dependenciesHelper.mockEducationalInstitutionCommandRepository.SetupSequence(r => r.GetEducationalInstitutionIncludingAdminsAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                                                                          .ReturnsAsync(testDataHelper.EducationalInstitutions[1])
+                                                                          .ReturnsAsync(testDataHelper.EducationalInstitutions[0]);
+        }
+
+        [Fact]
+        public async Task GivenAnUpdateEducationalInstitutionParentCommand_ToTransactionOperationsMethod_TheParentIDIsNotInDatabase_ShouldReturnAResponseThatIncludesAStatusCodeNotFoundField()
+        {
+            //Arrange
+            UpdateEducationalInstitutionParentCommand request = new()
+            {
+                EducationalInstitutionID = testDataHelper.EducationalInstitutions[0].Id,
+                ParentInstitutionID = Guid.NewGuid()
+            };
+
+            SetupMockedDependenciesToNotFindTheParent(request.ParentInstitutionID);
+
+            //Act
+            var result = await TransactionOperations(dependenciesHelper.mockTransaction.Object, dependenciesHelper.mockOutboxService.Object, request);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.NotFound, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task GivenAnUpdateEducationalInstitutionParentCommand_ToTransactionOperationsMethod_TheParentIDIsNotInDatabase_ShouldReturnAResponseThatIncludesAMessageField()
+        {
+            //Arrange
+            UpdateEducationalInstitutionParentCommand request = new()
+            {
+                EducationalInstitutionID = testDataHelper.EducationalInstitutions[0].Id,
+                ParentInstitutionID = Guid.NewGuid()
+            };
+
+            SetupMockedDependenciesToNotFindTheParent(request.ParentInstitutionID);
+
+            //Act
+            var result = await TransactionOperations(dependenciesHelper.mockTransaction.Object, dependenciesHelper.mockOutboxService.Object, request);
+
+            //Assert
+            Assert.Equal($"The Parent Educational Institution with the ID: {request.ParentInstitutionID} has not been found!", result.Message);
+        }
+
+        [Fact]
+        public async Task GivenAnUpdateEducationalInstitutionParentCommand_ToTransactionOperationsMethod_TheParentIDIsNotInDatabase_ShouldReturnAResponseThatIncludesAFalseOperationStatusField()
+        {
+            //Arrange
+            UpdateEducationalInstitutionParentCommand request = new()
+            {
+                EducationalInstitutionID = testDataHelper.EducationalInstitutions[0].Id,
+                ParentInstitutionID = Guid.NewGuid()
+            };
+
+            SetupMockedDependenciesToNotFindTheParent(request.ParentInstitutionID);
+
+            //Act
+            var result = await TransactionOperations(dependenciesHelper.mockTransaction.Object, dependenciesHelper.mockOutboxService.Object, request);
+
+            //Assert
+            Assert.False(result.OperationStatus);
+        }
+
+        private void SetupMockedDependenciesToNotFindTheParent(Guid parentId)
+        {
+            dependenciesHelper.mockUnitOfWorkCommand.Setup(uok => uok.UsingEducationalInstitutionCommandRepository())
+                                                    .Returns(dependenciesHelper.mockEducationalInstitutionCommandRepository.Object);
+
+            dependenciesHelper.mockEducationalInstitutionCommandRepository.Setup(r => r.GetEducationalInstitutionIncludingAdminsAsync(parentId, It.IsAny<CancellationToken>()))
+                                                                          .ReturnsAsync((Domain::EducationalInstitution)default);
+        }
+
+        [Fact]
+        public async Task GivenAnUpdateEducationalInstitutionParentCommand_ToTransactionOperationsMethod_TheEducationalInstitutionIsNotInDatabase_ShouldReturnAResponseThatIncludesAMessageField()
+        {
+            //Arrange
+            UpdateEducationalInstitutionParentCommand request = new()
+            {
+                EducationalInstitutionID = Guid.NewGuid(),
+                ParentInstitutionID = testDataHelper.EducationalInstitutions[1].Id
+            };
+
+            SetupMockedDependenciesToNotFindTheEducationalInstitution(request.EducationalInstitutionID, request.ParentInstitutionID);
+
+            //Act
+            var result = await TransactionOperations(dependenciesHelper.mockTransaction.Object, dependenciesHelper.mockOutboxService.Object, request);
+
+            //Assert
+            Assert.Equal($"The Educational Institution with the ID: {request.EducationalInstitutionID} has not been found!", result.Message);
+        }
+
+        [Fact]
+        public async Task GivenAnUpdateEducationalInstitutionParentCommand_ToTransactionOperationsMethod_TheEducationalInstitutionIsNotInDatabase_ShouldReturnAResponseThatIncludesAFalseOperationStatusField()
+        {
+            //Arrange
+            UpdateEducationalInstitutionParentCommand request = new()
+            {
+                EducationalInstitutionID = Guid.NewGuid(),
+                ParentInstitutionID = testDataHelper.EducationalInstitutions[1].Id
+            };
+
+            SetupMockedDependenciesToNotFindTheEducationalInstitution(request.EducationalInstitutionID, request.ParentInstitutionID);
+
+            //Act
+            var result = await TransactionOperations(dependenciesHelper.mockTransaction.Object, dependenciesHelper.mockOutboxService.Object, request);
+
+            //Assert
+            Assert.False(result.OperationStatus);
+        }
+
+        [Fact]
+        public async Task GivenAnUpdateEducationalInstitutionParentCommand_ToTransactionOperationsMethod_TheEducationalInstitutionIsNotInDatabase_ShouldReturnAResponseThatIncludesANotFoundStatusCodeField()
+        {
+            //Arrange
+            UpdateEducationalInstitutionParentCommand request = new()
+            {
+                EducationalInstitutionID = Guid.NewGuid(),
+                ParentInstitutionID = testDataHelper.EducationalInstitutions[1].Id
+            };
+
+            SetupMockedDependenciesToNotFindTheEducationalInstitution(request.EducationalInstitutionID, request.ParentInstitutionID);
+
+            //Act
+            var result = await TransactionOperations(dependenciesHelper.mockTransaction.Object, dependenciesHelper.mockOutboxService.Object, request);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.NotFound, result.StatusCode);
+        }
+
+        private void SetupMockedDependenciesToNotFindTheEducationalInstitution(Guid educationalInstitutionId, Guid parentId)
+        {
+            dependenciesHelper.mockUnitOfWorkCommand.Setup(uok => uok.UsingEducationalInstitutionCommandRepository())
+                                                   .Returns(dependenciesHelper.mockEducationalInstitutionCommandRepository.Object);
+
+            dependenciesHelper.mockEducationalInstitutionCommandRepository.SetupSequence(r => r.GetEducationalInstitutionIncludingAdminsAsync(It.IsIn(parentId, educationalInstitutionId), It.IsAny<CancellationToken>()))
+                                                                          .ReturnsAsync(testDataHelper.EducationalInstitutions[1])
+                                                                          .ReturnsAsync((Domain::EducationalInstitution)default);
+        }
+    }
+}
