d26ad401bd1bdfacca0c5c4091a993c24f5fa973
Vlad Bulhac <vladutbulhac@gmail.com>
2021-11-17 20:02:15 +0200

    added call to GetStatus(), refactored

--- src/APIGateway/Aggregator/Services/GrpcServiceBase.cs
+++ src/APIGateway/Aggregator/Services/GrpcServiceBase.cs
@@ -15,16 +15,25 @@ public abstract class GrpcServiceBase<TService> where TService : class
         try
         {
             var response = await call.ResponseAsync;
-            var trailers = call.GetTrailers();
 
             logger.LogDebug("gRPC call ended with response: {@response}", response);
 
-            return new() { Body = response, Trailers = trailers };
+            return new()
+            {
+                Body = response,
+                Status = call.GetStatus(),
+                Trailers = call.GetTrailers()
+            };
         }
         catch (Exception e)
         {
             logger.LogError($"Could not finish the gRPC call successfully, error details => {e.Message}");
-            return new() { Body = default, Trailers = new() };
+            return new()
+            {
+                Body = default,
+                Status = default,
+                Trailers = new()
+            };
         }
     }
 }
