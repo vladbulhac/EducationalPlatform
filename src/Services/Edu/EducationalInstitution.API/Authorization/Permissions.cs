5d1ab85c30be7129d6b9471847efabd9c5cba271
Vlad Bulhac <vladutbulhac@gmail.com>
2021-11-17 19:26:43 +0200

    added Permissions

---
+++ src/Services/Edu/EducationalInstitution.API/Authorization/Permissions.cs
@@ -0,0 +1,10 @@
+﻿namespace EducationalInstitutionAPI.Authorization
+{
+    public static class Permissions
+    {
+        public const string All = "user.educational_institution.all";
+        public const string UpdateDetails = "user.educational_institution.update_details";
+        public const string ChangeAdministrators = "user.educational_institution.change_administrators";
+        public const string Delete = "user.educational_institution.delete";
+    }
+}
