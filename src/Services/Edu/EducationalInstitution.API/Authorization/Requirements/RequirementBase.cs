7f893315be8b371d40ee9b810b3764d1f360a872
Vlad Bulhac <vladutbulhac@gmail.com>
2021-11-17 19:24:52 +0200

    added RequirementBase

---
+++ src/Services/Edu/EducationalInstitution.API/Authorization/Requirements/RequirementBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace EducationalInstitutionAPI.Authorization.Requirements
+{
+    public abstract class RequirementBase : IAuthorizationRequirement
+    {
+        public ResourcePolicy Policy { get; init; }
+
+        public RequirementBase(ResourcePolicy policy) => Policy = policy ?? throw new ArgumentNullException(nameof(policy));
+    }
+}
