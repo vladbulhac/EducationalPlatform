63e7177d0819e06afe3784f00671edd5f8e5a926
Vlad Bulhac <vladutbulhac@gmail.com>
2021-11-17 19:26:28 +0200

    added UpdateEducationalInstitutionRequirements

---
+++ src/Services/Edu/EducationalInstitution.API/Authorization/Requirements/UpdateEducationalInstitutionRequirements.cs
@@ -0,0 +1,10 @@
+﻿namespace EducationalInstitutionAPI.Authorization.Requirements;
+
+public class UpdateEducationalInstitutionRequirements : RequirementBase
+{
+    public UpdateEducationalInstitutionRequirements() : base(new UpdateEducationalInstitutionPolicy())
+    { }
+
+    public UpdateEducationalInstitutionRequirements(ResourcePolicy policy) : base(policy)
+    { }
+}
