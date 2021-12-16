f3d1253d4dddafe1d6ed267e31ce20b945ffe3ef
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-16 20:43:01 +0200

    configured SignalR, registered the new classes, added OpenIDDict configuration for authentication and authorization

--- src/Services/Notification/Notification.API/Startup.cs
+++ src/Services/Notification/Notification.API/Startup.cs
@@ -1,9 +1,15 @@
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using Notification.API.Hubs;
+using Notification.API.Hubs.Management;
 using Notification.Application.Integration_Events.Events;
 using Notification.Application.Integration_Events.Handlers;
+using Notification.Application.Services;
 using Notification.Infrastructure;
 using Notification.Infrastructure.Repositories;
+using OpenIddict.Validation.AspNetCore;
 using RabbitMQ.Client;
 using RabbitMQEventBus;
 using RabbitMQEventBus.Abstractions;
@@ -14,12 +20,13 @@ namespace Notification.API;
 
 public class Startup
 {
+    private readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+    public IConfiguration Configuration { get; }
+
     public Startup(IConfiguration configuration)
     {
         Configuration = configuration;
     }
-
-    public IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
@@ -35,12 +42,27 @@ public class Startup
             //options.LogTo(Console.WriteLine);
         });
 
+        services.AddOpenIDDictConfiguration(Configuration);
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy(MyAllowSpecificOrigins, builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyHeader()
+                       .AllowAnyMethod();
+            });
+        });
+
         services.AddTransient<INotificationRepository, NotificationRepository>();
+        services.AddTransient<INotificationService, NotificationService>();
+
+        services.AddSingleton<IUserIdProvider, ResourceOwnerIdentification>();
+        services.AddSignalR(options => { options.EnableDetailedErrors = true; });
+
+        services.AddMediatR(typeof(Startup), typeof(AssignedAdminsToEducationalInstitutionIntegrationEventHandler));
 
         services.AddEventBusConfiguration(Configuration);
-
-        services.AddControllers();
-        services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "Notification.API", Version = "v1" }));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,21 +71,25 @@ public class Startup
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Notification.API v1"));
         }
 
-        app.UseHttpsRedirection();
+        //app.UseHttpsRedirection();
+
+        app.UseCors(MyAllowSpecificOrigins);
 
         app.UseRouting();
 
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
         app.UseSerilogRequestLogging();
 
-        app.UseAuthorization();
+        app.UseAuthentication()
+           .UseAuthorization();
 
         app.UseEndpoints(endpoints =>
         {
-            endpoints.MapControllers();
+            endpoints.MapHub<NotificationHub>("/notifications");
         });
 
         app.AddEventBusSubscriptions();
@@ -121,5 +147,36 @@ public static class StartupExtensionMethods
 
         eventBus.Subscribe<NotifyAdminsOfEducationalInstitutionUpdateIntegrationEvent,
                            NotificationEventHandler<NotifyAdminsOfEducationalInstitutionUpdateIntegrationEvent>>();
+    }
+
+    public static IServiceCollection AddOpenIDDictConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication(options => options.DefaultAuthenticateScheme = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme);
+
+        services.AddOpenIddict()
+                .AddValidation(options =>
+                {
+                    // Note: the validation handler uses OpenID Connect discovery
+                    // to retrieve the address of the introspection endpoint.
+                    options.SetIssuer(configuration.GetSection("Identity")["Issuer"]);
+                    options.AddAudiences("notification_api");
+
+                    // Register the encryption credentials, a symmetric
+                    // encryption key that is shared between the server and all the resource servers
+                    // (that performs local token validation instead of using introspection).
+                    //
+                    // Note: in a real world application, this encryption key should be
+                    // stored in a safe place (e.g in Azure KeyVault, stored as a secret).
+                    options.AddEncryptionKey(new SymmetricSecurityKey(
+                            Convert.FromBase64String(configuration.GetSection("Identity")["SharedKey"])));
+
+                    // Register the System.Net.Http integration.
+                    options.UseSystemNetHttp();
+
+                    // Register the ASP.NET Core host.
+                    options.UseAspNetCore();
+                });
+
+        return services;
     }
 }
