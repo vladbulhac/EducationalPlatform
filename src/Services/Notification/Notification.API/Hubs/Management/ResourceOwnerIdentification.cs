d6f3d07edb069c8e54db4a72df85ecfaddfd9324
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-16 20:34:07 +0200

    added ResourceOwnerIdentification class

---
+++ src/Services/Notification/Notification.API/Hubs/Management/ResourceOwnerIdentification.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Notification.API.Hubs.Management;
+
+public class ResourceOwnerIdentification : IUserIdProvider
+{
+    public string GetUserId(HubConnectionContext connection)
+    {
+        return connection.User?.FindFirst("sub")?.Value;
+    }
+}
