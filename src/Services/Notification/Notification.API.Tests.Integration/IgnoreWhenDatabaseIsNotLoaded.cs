94c03cd45abfe17c8d15e8ff57ce6ce5fcf161d6
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-16 20:50:56 +0200

    added IgnoreWhenDatabaseIsNotLoaded

---
+++ src/Services/Notification/Notification.API.Tests.Integration/IgnoreWhenDatabaseIsNotLoaded.cs
@@ -0,0 +1,24 @@
+﻿using Notification.API.Tests.Shared;
+
+namespace Notification.API.Tests.Integration;
+
+/// <summary>
+/// Controls when Integration Tests that use an external database can be triggered
+/// </summary>
+public sealed class IgnoreWhenDatabaseIsNotLoaded : FactAttribute
+{
+    public IgnoreWhenDatabaseIsNotLoaded()
+    {
+        if (!IsDatabaseLoaded())
+            Skip = "Ignore when database is not loaded";
+    }
+
+    private static bool IsDatabaseLoaded()
+    {
+        var IS_DBLOADED = ConfigurationHelper.GetCurrentSettings(key: "Tests:IntegrationTestsVariables:IS_DBLOADED", configFilenames: "tests_config.json");
+        if (IS_DBLOADED is not null)
+            return bool.Parse(IS_DBLOADED);
+
+        return false;
+    }
+}
