597602a019cec4f6a90690caf9791c1ac70c918a
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-16 20:37:07 +0200

    added NotificationDispatcher class

---
+++ src/Services/Notification/Notification.API/NotificationHandlers/NotificationDispatcher.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.SignalR;
+using Notification.API.Hubs;
+using Notification.Application.DTOs;
+
+namespace Notification.API.NotificationHandlers;
+
+public class NotificationDispatcher : INotificationHandler<NotificationBody>
+{
+    private readonly ILogger<NotificationDispatcher> logger;
+    private readonly IHubContext<NotificationHub, INotificationHub> hub;
+
+    public NotificationDispatcher(IHubContext<NotificationHub, INotificationHub> hub, ILogger<NotificationDispatcher> logger)
+    {
+        this.hub = hub ?? throw new ArgumentNullException(nameof(hub));
+        this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async Task Handle(NotificationBody notification, CancellationToken cancellationToken = default)
+    {
+        logger.LogDebug("Sending message {0} to {1}.", notification.Title, notification.To);
+
+        await hub.Clients.Group(notification.To).ReceiveNotification(notification);
+    }
+}
