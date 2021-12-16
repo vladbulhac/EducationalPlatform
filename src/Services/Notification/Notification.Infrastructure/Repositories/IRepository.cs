5d93705169519e3a8a1f89d0c27dd939e361ddd1
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-16 20:19:18 +0200

    IRepository implements IDisposable, added GetAsync method, added recipientId parameter to GetByIdAsync method

--- src/Services/Notification/Notification.Infrastructure/Repositories/IRepository.cs
+++ src/Services/Notification/Notification.Infrastructure/Repositories/IRepository.cs
@@ -1,15 +1,18 @@
 ﻿using Notification.Domain.Building_Blocks;
+using Notification.Domain.Models.Aggregates;
 
 namespace Notification.Infrastructure.Repositories;
 
 /// <summary>
-/// Encapsulates the logic required to access data sources
+/// Encapsulates the logic required to access data sources.
 /// </summary>
-public interface IRepository<T> where T : IAggregateRoot
+public interface IRepository<T> : IDisposable where T : IAggregateRoot
 {
     public Task AddAsync(T entity, CancellationToken cancellationToken = default);
 
-    public Task<T> GetByIdAsync(string id, CancellationToken cancellationToken = default);
+    public Task<T> GetByIdAsync(string eventId, string recipientId, CancellationToken cancellationToken = default);
+
+    public Task<ICollection<Event>> GetAsync(string recipientId, int offset = 0, int resultsCount = 50, CancellationToken cancellationToken = default);
 
     public Task SaveChangesAsync(CancellationToken cancellationToken = default);
 }
