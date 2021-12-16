160ad7cba0be95124a208b71c7cfc9fbb6cdc0a6
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-16 20:48:06 +0200

    added MockDependenciesHelper class

---
+++ src/Services/Notification/Notification.API.Tests.Unit/MockDependenciesHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using Notification.API.Hubs;
+using Notification.Application.Services;
+using Notification.Infrastructure.Repositories;
+
+namespace Notification.API.Tests.Unit;
+
+/// <summary>
+/// Contains a collection of mocked types used in unit testing.
+/// </summary>
+public class MockDependenciesHelper<T> where T : class
+{
+    public readonly Mock<ILogger<T>> mockLogger;
+
+    public Mock<INotificationHub> mockClientOne;
+    public Mock<INotificationHub> mockClientTwo;
+    public Mock<IHubCallerClients<INotificationHub>> mockHubClients;
+
+    public readonly Mock<INotificationService> mockNotificationService;
+
+    public readonly Mock<INotificationRepository> mockNotificationRepository;
+
+    public MockDependenciesHelper()
+    {
+        mockLogger = new();
+
+        mockClientOne = new();
+        mockClientTwo = new();
+        mockHubClients = new();
+
+        mockNotificationService = new();
+
+        mockNotificationRepository = new();
+    }
+}
