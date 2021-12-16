641a4a0b36588cb759c1a8f0b2e2f5d13643b1e4
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-16 20:33:41 +0200

    added notification dtos

---
+++ src/Services/Notification/Notification.API/Hubs/DataTransferObjects/NotificationDTOs.cs
@@ -0,0 +1,17 @@
+﻿namespace Notification.API.Hubs.DataTransferObjects;
+
+public record GetNotificationDTO
+{
+    public string EventId { get; init; }
+}
+
+public record GetNotificationsDTO
+{
+    public ICollection<string> EventsIds { get; init; }
+}
+
+public record GetNotificationsOfRecipientDTO
+{
+    public int Offset { get; init; }
+    public int ResultsCount { get; init; }
+}
