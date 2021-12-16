92a0cd4e1c5b7bb959f987479a96504d5115bf41
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-16 20:22:17 +0200

    NotificationRepository implements IDisposable, GetEventsFromCollectionAsync and GetAsync methods

--- src/Services/Notification/Notification.Infrastructure/Repositories/NotificationRepository.cs
+++ src/Services/Notification/Notification.Infrastructure/Repositories/NotificationRepository.cs
@@ -1,34 +1,62 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Notification.Domain.Models.Aggregates;
-using Notification.Infrastructure.Repositories.Results;
 
 namespace Notification.Infrastructure.Repositories;
 
+/// <summary>
+/// <inheritdoc cref="INotificationRepository"/>
+/// </summary>
 public class NotificationRepository : INotificationRepository
 {
+    private bool disposed;
+
     private readonly NotificationContext context;
 
     public NotificationRepository(NotificationContext context)
-        => this.context = context ?? throw new ArgumentNullException(nameof(context));
+            => this.context = context ?? throw new ArgumentNullException(nameof(context));
 
-    public async Task AddAsync(Event eventEntity, CancellationToken cancellationToken = default) => await context.AddAsync(eventEntity, cancellationToken);
+    public async Task AddAsync(Event eventEntity, CancellationToken cancellationToken = default)
+            => await context.AddAsync(eventEntity, cancellationToken);
 
-    public async Task<Event> GetByIdAsync(string eventId, CancellationToken cancellationToken = default)
-            => await context.Events.Include(e => e.Recipients)
-                                   .SingleOrDefaultAsync(e => e.Id == eventId, cancellationToken);
+    public async Task<Event> GetByIdAsync(string eventId, string recipientId, CancellationToken cancellationToken = default)
+            => await context.Events.Include(e => e.Recipients.Where(r => r.Id == recipientId))
+                                   .SingleOrDefaultAsync(e => e.Id == eventId &&
+                                                              e.Recipients.Select(r => r.Id).Contains(recipientId),
+                                                         cancellationToken);
 
-    public async Task SaveChangesAsync(CancellationToken cancellationToken = default) => await context.SaveChangesAsync(cancellationToken);
+    public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+            => await context.SaveChangesAsync(cancellationToken);
 
-    public async Task<ICollection<GetEventDetails>> GetUnseenEventsForRecipientAsync(string recipientID, CancellationToken cancellationToken = default)
+    public async Task<ICollection<Event>> GetAsync(string recipientId, int offset = 0, int resultsCount = 50, CancellationToken cancellationToken = default)
+            => await context.Events.Include(e => e.Recipients.Where(r => r.Id == recipientId))
+                                   .Where(e => e.Recipients.Select(r => r.Id).Contains(recipientId))
+                                   .OrderBy(r => r.TriggerDetails.TimeIssued)
+                                   .Skip(offset)
+                                   .Take(resultsCount)
+                                   .ToListAsync(cancellationToken);
+
+    public async Task<ICollection<Event>> GetEventsFromCollectionAsync(ICollection<string> eventsIds, string recipientId, CancellationToken cancellationToken = default)
+            => await context.Events.Include(e => e.Recipients.Where(r => r.Id == recipientId))
+                                   .Where(e => eventsIds.Contains(e.Id) && e.Recipients.Select(r => r.Id).Contains(recipientId))
+                                   .OrderBy(e => e.TriggerDetails.TimeIssued)
+                                   .ToListAsync(cancellationToken);
+
+    protected virtual void Dispose(bool disposing)
     {
-        return await context.Recipients.Include(r => r.Event)
-                                       .Where(r => r.Id == recipientID && !r.Seen)
-                                       .Select(e => new GetEventDetails()
-                                       {
-                                           Message = e.Event.Message,
-                                           TimeIssued = e.Event.TriggerDetails.TimeIssued,
-                                           Url = e.Event.Uri
-                                       })
-                                       .ToListAsync(cancellationToken);
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                context.Dispose();
+            }
+
+            disposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
     }
 }
