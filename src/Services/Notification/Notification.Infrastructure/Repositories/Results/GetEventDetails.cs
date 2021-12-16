ffa22c7e3dbe8f7606d832a06c386339b29f0125
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-16 20:15:38 +0200

    removed GetEventDetails record

--- src/Services/Notification/Notification.Infrastructure/Repositories/Results/GetEventDetails.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Notification.Infrastructure.Repositories.Results
-{
-    public record GetEventDetails
-    {
-        public DateTime TimeIssued { get; init; }
-        public string Message { get; init; }
-        public string Url { get; init; }
-    }
-}
