e21fe14f401d028c765605bc6299e3c28ee8e2d6
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-16 20:32:23 +0200

    added MediatR's mediator to send the notifications to the NotificationHandlers

--- src/Services/Notification/Notification.Application/Integration Events/Handlers/AssignedAdminsToEducationalInstitutionIntegrationEventHandler.cs
+++ src/Services/Notification/Notification.Application/Integration Events/Handlers/AssignedAdminsToEducationalInstitutionIntegrationEventHandler.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Notification.Application.DTOs;
 using Notification.Application.Integration_Events.Events;
 using Notification.Domain.Models.Aggregates;
 using Notification.Infrastructure.Repositories;
@@ -8,21 +10,28 @@ namespace Notification.Application.Integration_Events.Handlers;
 
 public class AssignedAdminsToEducationalInstitutionIntegrationEventHandler : IIntegrationEventHandler<AssignedAdminsToEducationalInstitutionIntegrationEvent>
 {
+    private readonly IMediator mediator;
     private readonly INotificationRepository repository;
     private readonly ILogger<AssignedAdminsToEducationalInstitutionIntegrationEventHandler> logger;
 
-    public AssignedAdminsToEducationalInstitutionIntegrationEventHandler(INotificationRepository repository, ILogger<AssignedAdminsToEducationalInstitutionIntegrationEventHandler> logger)
+    public AssignedAdminsToEducationalInstitutionIntegrationEventHandler(INotificationRepository repository, IMediator mediator, ILogger<AssignedAdminsToEducationalInstitutionIntegrationEventHandler> logger)
     {
         this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
         this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
     }
 
+    /// <summary>
+    /// Saves to the database the received event and sends it as notifications to <see cref="INotificationHandler{TNotification}"/>.
+    /// </summary>
     public async Task HandleEvent(AssignedAdminsToEducationalInstitutionIntegrationEvent @event)
     {
         logger.LogInformation($"Received integration event: {@event} !");
 
         try
         {
+            var notifications = new List<NotificationBody>(@event.NewAdmins.Count);
+
             foreach (var adminDetails in @event.NewAdmins)
             {
                 Event eventEntity = new(@event.GetType().Name,
@@ -34,9 +43,14 @@ public class AssignedAdminsToEducationalInstitutionIntegrationEventHandler : IIn
                                         adminDetails.Identity,
                                         @event.EventId);
 
+                notifications.Add(new NotificationBody(eventEntity, adminDetails.Identity));
+
                 await repository.AddAsync(eventEntity);
                 await repository.SaveChangesAsync();
             }
+
+            foreach (var notification in notifications)
+                await mediator.Publish(notification);
         }
         catch (Exception e)
         {
--- src/Services/Notification/Notification.Application/Integration Events/Handlers/NotificationEventHandler.cs
+++ src/Services/Notification/Notification.Application/Integration Events/Handlers/NotificationEventHandler.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Notification.Application.DTOs;
 using Notification.Application.Integration_Events.Events;
 using Notification.Domain.Models.Aggregates;
 using Notification.Infrastructure.Repositories;
@@ -8,15 +10,20 @@ namespace Notification.Application.Integration_Events.Handlers;
 
 public class NotificationEventHandler<TEvent> : IIntegrationEventHandler<TEvent> where TEvent : NotificationIntegrationEvent
 {
+    private readonly IMediator mediator;
     private readonly INotificationRepository repository;
     private readonly ILogger<NotificationEventHandler<TEvent>> logger;
 
-    public NotificationEventHandler(INotificationRepository repository, ILogger<NotificationEventHandler<TEvent>> logger)
+    public NotificationEventHandler(INotificationRepository repository, IMediator mediator, ILogger<NotificationEventHandler<TEvent>> logger)
     {
         this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
         this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
     }
 
+    /// <summary>
+    /// Saves to the database the received event and sends it as notifications to <see cref="INotificationHandler{TNotification}"/>.
+    /// </summary>
     public async Task HandleEvent(TEvent @event)
     {
         logger.LogInformation($"Received integration event: {@event} !");
@@ -24,7 +31,8 @@ public class NotificationEventHandler<TEvent> : IIntegrationEventHandler<TEvent>
         try
         {
             Event eventEntity = new(@event.GetType().Name,
-                                    @event.Message, @event.Uri,
+                                    @event.Message,
+                                    @event.Uri,
                                     @event.TimeIssued,
                                     @event.TriggeredBy.Action,
                                     @event.TriggeredBy.ServiceName,
@@ -33,6 +41,9 @@ public class NotificationEventHandler<TEvent> : IIntegrationEventHandler<TEvent>
 
             await repository.AddAsync(eventEntity);
             await repository.SaveChangesAsync();
+
+            foreach (var recipient in @event.ToNotify)
+                await mediator.Publish(new NotificationBody(eventEntity, recipient));
         }
         catch (Exception e)
         {
