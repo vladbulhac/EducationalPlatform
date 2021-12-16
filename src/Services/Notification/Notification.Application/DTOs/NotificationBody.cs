17b719ae76e73ba09d00b42a97e20368da2a713e
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-16 20:29:05 +0200

    added NotificationBody class

---
+++ src/Services/Notification/Notification.Application/DTOs/NotificationBody.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Notification.Application.Extensions;
+using Notification.Domain.Models.Aggregates;
+
+namespace Notification.Application.DTOs;
+
+public class NotificationBody : INotification
+{
+    public string Id { get; init; }
+    public string Title { get; init; }
+    public string From { get; init; }
+    public string To { get; init; }
+    public string Message { get; init; }
+    public DateTime IssuedAt { get; init; }
+    public string Uri { get; init; }
+    public bool Seen { get; init; }
+
+    public NotificationBody(Event @event, string to)
+    {
+        Id = @event.Id;
+        Title = @event.Name.AddSpacesAfterWords();
+
+        From = @event.TriggerDetails.Issuer;
+        To = to ?? throw new ArgumentNullException(nameof(to));
+
+        Message = @event.Message;
+        Uri = @event.Uri;
+
+        IssuedAt = @event.TriggerDetails.TimeIssued;
+
+        Seen = @event.Recipients.First(r => r.Id == to).Seen;
+    }
+}
