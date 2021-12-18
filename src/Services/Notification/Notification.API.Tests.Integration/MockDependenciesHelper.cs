166973370f4ef7acd57aa27e60fb8029bff55c16
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-18 21:09:17 +0200

    added MockDependenciesHelper to Notification.API.Tests.Integration

---
+++ src/Services/Notification/Notification.API.Tests.Integration/MockDependenciesHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Notification.API.Tests.Integration;
+
+/// <summary>
+/// Contains a collection of mocked types used in unit testing.
+/// </summary>
+public class MockDependenciesHelper<T> where T : class
+{
+    public readonly Mock<ILogger<T>> mockLogger;
+
+    public MockDependenciesHelper()
+    {
+        mockLogger = new();
+    }
+}
