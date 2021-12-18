59190e7f98aef2ef24481c856866ed7c46eb8c42
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-18 21:22:20 +0200

    added HubCallFailDetails to Notification.API

---
+++ src/Services/Notification/Notification.API/Hubs/DataTransferObjects/HubCallFailDetails.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace Notification.API.Hubs.DataTransferObjects;
+
+public record HubCallFailDetails
+{
+    public HttpStatusCode StatusCode { get; init; }
+    public string Message { get; init; }
+
+    public HubCallFailDetails(HttpStatusCode statusCode, string message)
+    {
+        StatusCode = statusCode;
+        Message = message ?? string.Empty;
+    }
+}
