77837ae1ae7eba23ecdafefe0b6f068fc2b49bec
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-18 21:22:42 +0200

    added IHubCall to Notification.API

---
+++ src/Services/Notification/Notification.API/Hubs/Management/IHubCall.cs
@@ -0,0 +1,8 @@
+﻿using Notification.API.Hubs.DataTransferObjects;
+
+namespace Notification.API.Hubs.Management;
+
+public interface IHubCall
+{
+    public Task CallFailed(HubCallFailDetails details);
+}
