5a2088c46898348802a797a4a8b201d69821681d
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-18 21:00:46 +0200

    renamed GetNotifications interface to GetNotificationsOfRecipientDTO

--- src/Services/Notification/SignalRTestClient/src/app/dtos/GetNotifications.dto.ts
+++
@@ -1,4 +0,0 @@
-export interface GetNotifications{
-    Offset:number;
-    ResultsCount:number;
-}
---
+++ src/Services/Notification/SignalRTestClient/src/app/dtos/GetNotificationsOfRecipient.dto.ts
@@ -0,0 +1,4 @@
+export interface GetNotificationsOfRecipientDTO {
+  Offset: number;
+  ResultsCount: number;
+}
