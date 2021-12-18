0cb047673828126d68f7351b107b6b750dadcef3
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-18 21:13:19 +0200

    added GetNotificationDTOValidator to Notification.API

---
+++ src/Services/Notification/Notification.API/Hubs/DataTransferObjects/Validators/GetNotificationDTOValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Notification.API.Hubs.DataTransferObjects.Validators;
+
+public class GetNotificationDTOValidator : AbstractValidator<GetNotificationDTO>
+{
+    public GetNotificationDTOValidator()
+    {
+        CascadeMode = CascadeMode.Stop;
+        RuleFor(dto => dto.EventId).NotEmpty()
+                                   .WithMessage("{PropertyName} was empty or null!");
+    }
+}
