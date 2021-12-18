7986ce6ba6d2b370a8016a8934960c9655d292d2
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-18 21:15:04 +0200

    added SeenNotificationsDTOValidator to Notification.API

---
+++ src/Services/Notification/Notification.API/Hubs/DataTransferObjects/Validators/SeenNotificationsDTOValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Notification.API.Hubs.DataTransferObjects.Validators;
+
+public class SeenNotificationsDTOValidator : AbstractValidator<SeenNotificationsDTO>
+{
+    public SeenNotificationsDTOValidator()
+    {
+        CascadeMode = CascadeMode.Stop;
+
+        RuleFor(dto => dto.EventsIds).NotEmpty()
+                                     .WithMessage("{PropertyName} was empty or null!");
+
+        RuleForEach(dto => dto.EventsIds).NotEmpty()
+                                         .WithMessage("An id was empty or null!");
+    }
+}
