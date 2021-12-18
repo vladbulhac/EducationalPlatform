bfca52a48a17d652823a422d180e7d59f02e3edb
Vlad Bulhac <vladutbulhac@gmail.com>
2021-12-18 21:14:15 +0200

    added GetNotificationsOfRecipientDTOValidator to Notification.API

---
+++ src/Services/Notification/Notification.API/Hubs/DataTransferObjects/Validators/GetNotificationsOfRecipientDTOValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Notification.API.Hubs.DataTransferObjects.Validators;
+
+public class GetNotificationsOfRecipientDTOValidator : AbstractValidator<GetNotificationsOfRecipientDTO>
+{
+    public GetNotificationsOfRecipientDTOValidator()
+    {
+        CascadeMode = CascadeMode.Stop;
+
+        RuleFor(dto => dto.Offset).GreaterThanOrEqualTo(0)
+                                  .WithMessage("{PropertyName} was an incorrect value! It must be a value greater or equal to 0!");
+
+        RuleFor(dto => dto.ResultsCount).GreaterThanOrEqualTo(1)
+                                      .WithMessage("{PropertyName} was an incorrect value! It must be a value greater or equal to 1!");
+    }
+}
